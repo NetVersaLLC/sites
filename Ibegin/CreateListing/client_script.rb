e46d47348b84922689d2ca5020013e452d6368bf
JoshWillis <blue74dot@gmail.com>
2013-05-01 22:06:15 +0000

    Fix for Ibegin Categories with special characters

--- Ibegin/CreateListing/client_script.rb
+++ Ibegin/CreateListing/client_script.rb
@@ -6,25 +6,41 @@ sign_in( data )
 sleep(1)
 @browser.select_list( :id, 'region').select data['state_name']
 sleep(1)
-@browser.select_list( :id, 'city').options(:text => /#{data['city']}/i).click
+@browser.select_list( :id, 'city').option(:text => /#{data['city']}/i).click
 @browser.text_field( :name, 'address').set data['address']
 @browser.text_field( :name, 'zip').set data['zip']
 @browser.text_field( :name, 'phone').set data['phone']
 @browser.text_field( :name, 'fax').set data['fax']
 
-
-
+query = data[ 'category1' ]
+wordcount = data[ 'category1' ].split(" ").size
+count = 0
 #Categories
 #Category selection involves a popup window that must be attached to than the category searched for, then selected.
-@browser.div( :id => 'id_category1_wrap', :index => 0 ).link( :title, 'Select' ).click
-@browser.window( :title, "Categories Selector | iBegin").when_present.use do
-sleep(3)
-	@browser.text_field( :id, 'id_q').set data[ 'category1' ]
-	@browser.button( :value, 'Go').click
-	sleep(2)
-	@browser.link( :text => /#{data[ 'category1' ]}/i).click
-end
+# If the category cannot be found it will split the category into single words and search word by word until it finds the category
+begin
+	@browser.div( :id => 'id_category1_wrap', :index => 0 ).link( :title, 'Select' ).click
+	@browser.window( :title, "Categories Selector | iBegin").when_present.use do
+		sleep(3)
+		@browser.text_field( :id, 'id_q').set query
+		@browser.button( :value, 'Go').click
+		sleep(2)
+		@browser.link( :text => /#{data[ 'category1' ]}/i).click
+		
+	end
+rescue Watir::Exception::UnknownObjectException
+	if wordcount > 0
+		query = data[ 'category1' ].split(" ")[count]
+		count += 1
+		retry
+	else
+		throw "Could not find the specified category: #{data['category1']}"
+	end
 
+rescue Exception => e
+    puts(e.inspect)
+
+end
 
 data[ 'payment_methods' ].each{ | method |
     @browser.checkbox( :id => /#{method}/ ).click

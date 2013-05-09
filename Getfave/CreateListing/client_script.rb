f0f6ded26a11f6643a4d1567e9db3f4c3ff5d916
JoshWillis <blue74dot@gmail.com>
2013-05-09 17:21:36 +0000

    Fixes for Getfave

--- Getfave/CreateListing/client_script.rb
+++ Getfave/CreateListing/client_script.rb
@@ -6,24 +6,30 @@
 sleep(1)
 @browser.button(:xpath => '//*[@id="signin-container"]/form/input[3]').click
 
+sleep 10
 
-sleep(100000)
-@browser.link(:id,'change-location').when_present.flash
 @browser.link(:id,'change-location').when_present.click
 @browser.text_field(:id, 'g-text-field').set data[ 'city' ] + ", " + data[ 'state' ]
-@browser.button(:value,'Pin').click
+sleep 3
+
+@browser.text_field(:id, 'g-text-field').send_keys :enter
+@browser.send_keys :enter
 #@browser.button(:value,'Pin').click
-sleep(2)
+#@browser.button(:value,'Pin').click
+sleep 10
 queryurl = "https://www.getfave.com/search?utf8=%E2%9C%93&q=" + data[ 'bus_name_fixed' ]
 puts(queryurl)
 @browser.goto(queryurl)
 
 Watir::Wait.until { @browser.div(:id,'results').exists? }
+
 @results = @browser.div(:id,'results') 
 @result_msg = "We couldn't find any matches."
 @matching_result = @browser.div(:id,'business-results').span(:text,"#{data[ 'business' ] }")
+
 if @results.exists? && @results.text.include?(@result_msg) || @matching_result.exist? == false
 #Add business
+
 	@browser.span(:text,'Add your business to Fave').click
 	@browser.text_field( :id => 'business_name' ).set data[ 'business' ]
 	@browser.text_field( :id => 'business_location' ).set data[ 'address' ]
@@ -38,7 +44,10 @@ if @results.exists? && @results.text.include?(@result_msg) || @matching_result.e
 	@browser.text_field( :id => 'business_email' ).set data[ 'business_email' ]
 	@browser.text_field( :id => 'business_hours' ).set data[ 'business_hours' ]
 	@browser.button(:value,'Publish Changes').click
-	
+
 true
 
+else
+	puts("Business is already listed.")
+	true
 end

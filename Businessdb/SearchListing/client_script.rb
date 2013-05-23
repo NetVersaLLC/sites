044be46e50d012278959256a2d0c9e404025f425
amitr1983 <amitr1983@github.com>
2013-05-23 21:01:08 +0000

    Fixing story:50273773

--- Businessdb/SearchListing/client_script.rb
+++ Businessdb/SearchListing/client_script.rb
@@ -3,3 +3,9 @@ if search_business(data)
   puts "Business Already Exist"
   true
 end
+
+#Main steps
+url = 'http://www.businessdb.com/'
+@browser.goto(url)
+# Search busienss
+search_business(data)
--- Businessdb/shared.rb
+++ Businessdb/shared.rb
@@ -1,16 +1,18 @@
 def search_business(data)
-  $matching_result = false
+  businessFound = [:unlisted]
   @browser.text_field(:id=> 'home-search-txt').clear
-  @browser.text_field(:id=> 'home-search-txt').set data[ 'location' ] 
+  @browser.text_field(:id=> 'home-search-txt').set data[ 'location' ]
   @browser.text_field(:id=> 'home-search-txt').send_keys(:return)
   list = @browser.div(:class=>'nine columns', :index=>1).ul(:class=>'social-list')
-   if list.exist? 
+   if list.exist?
      list.lis.each do |li|
        if li.text.include?(data[ 'business' ])
-         $matching_result = true
+         businessFound = [:listed, :unclaimed]    
          break
        end
      end
+     else
+	 businessFound = [:unlisted]  
   end
-  return $matching_result
+  return businessFound
 end

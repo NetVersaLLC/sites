77e3cde300f78a118799db730a776120aea8e16c
Lei Wang <leiwang1688@yahoo.com>
2013-05-25 07:31:13 -0700

    modify Localcom client_script.rb

--- Localcom/SearchListing/client_script.rb
+++ Localcom/SearchListing/client_script.rb
@@ -1,31 +1,37 @@
-url = "http://www.local.com/results/?keyword=#{data['businessfixed']}&location=#{data['city']}%252C%2520#{data['state_short']}"
+url = "http://www.local.com/business/results/?keyword=#{CGI.escape(data['business'])}&location=#{CGI.escape(data['city'] + ', ' + data['state_short'])}"
 
-page = Nokogiri::HTML(RestClient.get(url))  
-thelist = page.css("div.listBlockL")
+businessFound = {'status' => :unlisted} 
+
+page = Nokogiri::HTML(RestClient.get(url)) 
+
+thelist = page.css(".courtesyListing")
 
 thelist.each do |item|
-
   thelink = item.css("a.txtBlock.item.orgClick")
-  #puts(thelink[0].to_s)
-  
+
   next if thelink.to_s == ""
   next if not thelink[0]['href'] =~ /\/business\/details\//i
   
   if thelink.text.to_s =~ /#{data['business']}/i
-    
-    puts(thelink[0]['href'])
+    businessFound['status'] = :listed
+
+    street_addr = item.css('.street-address.fl').text.strip
+    loc_addr = item.css('.locality.region.fl').text.strip
+    businessFound['listed_address'] = street_addr + loc_addr
+    businessFound['listed_phone'] = item.css('.phoneNumber.tel').text.strip 
+
     subpage = Nokogiri::HTML(RestClient.get("http://www.local.com"+thelink[0]['href']))
     if subpage.css("a.blueLink.bold").length == 0
-      businessFound = [:listed, :claimed]      
+      businessFound['status'] = :unclaimed      
     else
-      businessFound = [:listed, :unclaimed]
+      businessFound['status'] = :claimed
     end
     
     break
   end  
-  businessFound = [:unlisted]
+  
 end
 
 
 STDERR.puts businessFound.inspect
-return true, businessFound
+[true, businessFound]

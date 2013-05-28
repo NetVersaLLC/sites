fade8454439d3f69f8b0e4edfc5cdc00aae7969b
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-05-28 00:50:38 +0000

    SearchListing works now

--- Cornerstonesworld/SearchListing/client_script.rb
+++ Cornerstonesworld/SearchListing/client_script.rb
@@ -1,14 +1,45 @@
+
 url = "http://www.cornerstonesworld.com/en/directory/country/USA/keyword/#{data['businessfixed']}/zip/#{data['zip']}/new"
-page = Nokogiri::HTML(RestClient.get(url))  
+puts(url)
+puts("Navigated Successfully")
+page = Nokogiri::HTML(RestClient.get(url))
+puts("page Assigned")
 firstItem = page.css("span.titlesmalldblue")
+puts("firstItem Assigned")
+
+#Unneeded Watir rescue script, incomplete
+#rescue
+#	@browser.goto('www.cornerstonesworld.com')
+#	puts("Arrived at website")
+#	@browser.text_field(:name, 'kw').set data['business']
+#	puts("Keyword Complete")
+#	@browser.select_list(:id, 'cn').select data['country']
+#	puts("Country Selected")
+#	@browser.text_field(:id, 'zip').set data['zip']
+#	puts("Zip Complete")
+#	@browser.button(:name, 'sbm').click
+#	puts("Submitted")
+
+#end
+#begin
+# if @browser.table(:class, 'dirlist').include? data['business'] then
+#	puts("Business Found")
+#	businessFound = [:listed, :unclaimed]
+# else
+# 	puts("Business unlisted")
+#   businessFound = [:unlisted]
+#end
 
 if firstItem.length == 0
   businessFound = [:unlisted]
+  puts("Unlisted")
 else
    if firstItem.text == data['business']
-      businessFound = [:listed, :unclaimed]     
+      businessFound = [:listed, :unclaimed]
+      puts("Listed, Uncalimed")     
    else
-      businessFound = [:unlisted]      
+      businessFound = [:unlisted]
+      puts("Unlisted2")      
    end  
 end
 

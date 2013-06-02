c0d8008d5b49dd6d91371e6f280776e6fe3fdb4f
JoshWillis <blue74dot@gmail.com>
2013-06-02 15:42:38 +0000

    Fixed Bing/SearchListing

--- Bing/SearchListing/client_script.rb
+++ Bing/SearchListing/client_script.rb
@@ -1,25 +1,43 @@
-agent = Mechanize.new
-agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
-agent.user_agent_alias = 'Mac Safari'
-page = agent.get("https://www.bingbusinessportal.com/Node:%5BApplication%5D%5C%5CStructure%5C%5CContent%5C%5CLiveLookups%5C%5CAddress%20Book%20Lookup.GetObjectListDataAsJSON?aDataTypeList=Asset%2CBusiness%2CAddress%2CCity%2CStateOrProvince%2CPostalCode%2CPhoneNumber%2CUniqueID%2CCountryOrRegion&aUse=LiveLookup&aCountSubNodes=Y&aSourceFilter=JSON%3A%5B%7B%22DataType%22%3A%22Business%22%2C%22Condition%22%3A%22Equal%20To%22%2C%22Value%22%3A%5B%7B%22Text%22%3A%22#{data['business']}%22%7D%5D%7D%2C%7B%22DataType%22%3A%22Address%22%2C%22Condition%22%3A%22Equal%20To%22%2C%22Value%22%3A%5B%7B%22Text%22%3A%22#{data['zip']}%22%7D%5D%7D%5D&aResultCount=500")
-businessFound = [:unlisted]
-meta = {}
-obj = JSON.parse(page.body)
-obj.each do |siness|
-  if siness['Business'] =~ /#{data['business']}/
-    claimed = RestClient.get "https://www.bingbusinessportal.com/Node:%5BApplication%5D%5C%5CStructure%5C%5CContent%5C%5CLiveLookups%5C%5CLocal%20Businesses.GetObjectListDataAsJSON?aDataTypeList=Ownership%20State&aUse=LiveLookup&aCountSubNodes=Y&aSourceFilter=JSON%3A%5B%7B%22DataType%22%3A%22YPID%22%2C%22Condition%22%3A%22Equal%20To%22%2C%22Value%22%3A%5B%7B%22Text%22%3A%22#{siness['UniqueID']}%22%7D%5D%7D%5D"
-    isclaimed = JSON.parse(claimed)
-    if isclaimed[0]
-      businessFound = [:listed, :claimed]
-    else
-      businessFound = [:listed, :unclaimed]
+def search_for_business( business )
+
+businessFound = {}
+  @browser.goto( 'http://www.bing.com/businessportal/' ) 
+  sleep 2
+  @browser.button( :value , 'Get Started' ).when_present.click
+  sleep 2
+  @browser.link(:title => 'Add Your Business').when_present.click
+  
+
+  
+    sleep 2
+    @browser.execute_script("hidePopUp()")
+    @browser.text_field(:name => 'PhoneNumber').when_present.set business['phone']
+    @browser.execute_script("hidePopUp()")
+    @browser.button(:value => 'Search').click
+    @browser.execute_script("hidePopUp()")
+    sleep 2
+    Watir::Wait.until(10) { @browser.text.include? "Search Results" or @browser.text.include? "We found no businesses with the given information"}
+
+    sleep 10
+
+    @browser.links.each do |nk|
+      next if not nk.attribute_value("title")
+      next if not nk.visible?
+      puts "Incoming link title:"
+      puts nk.attribute_value("title")
+
     end
-    meta['name'] = siness['Business']
-    meta['address'] = siness['Address']
-    meta['phone'] = siness['PhoneNumber']
-    businessFound.push meta
-    break
-  end
+
+    if @browser.link(:title => /#{business['business']}/i).exists?
+      businessFound['status'] = :listed
+      businessFound['listed_name'] = @browser.link(:title => /#{business['business']}/i).text
+      businessFound['listed_address'] = @browser.p(:xpath => '//*[@id="0"]/tbody/tr/td[2]/p[1]').text + @browser.p(:xpath => '//*[@id="0"]/tbody/tr/td[2]/p[2]').text
+    else
+      businessFound['status'] = :unlisted
+    end
+  return businessFound
 end
+
+businessFound = search_for_business(data)
 
 [true, businessFound]

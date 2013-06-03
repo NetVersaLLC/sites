40591ac1fdb4b41bf070179fd89802aee6a0c06b
Jonathan Jeffus <jjeffus@gmail.com>
2013-06-03 12:13:08 -0700

    adding google scan

---
+++ Google/SearchListing/client_script.rb
@@ -0,0 +1,30 @@
+api_key = 'AIzaSyC4ZArgxm2rBr_PqGCIwug0iRqil8Ac9E4'
+
+latlon = "#{data['latitude']},#{data['longitude']}"
+
+url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{latlon}&radius=50000&keyword=#{CGI.escape(data['business'])}&sensor=false&key=#{api_key}"
+
+res = RestClient.get  url
+results = JSON.parse(res)
+
+businessFound = {
+  :status => :unlisted
+}
+
+if results['results'] and results['results'].length > 0
+  results['results'].each do |result|
+    if result['name'] =~ /#{data['business']}/i
+      businessFound = {
+	:status => :listed
+      }
+      res = RestClient.get "https://maps.googleapis.com/maps/api/place/details/json?reference=#{result['reference']}&sensor=true&key=#{api_key}"
+      place = JSON.parse( res )
+      businessFound['listed_phone'] = place['result']['formatted_phone_number']
+      businessFound['listed_address'] = place['result']['formatted_address']
+      businessFound['listed_url'] = place['result']['url']
+      break
+    end
+  end
+end
+
+[true, businessFound]

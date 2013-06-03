4959e747fb816659812fa116795e80853c1e1cda
JoshWillis <blue74dot@gmail.com>
2013-06-03 21:13:03 +0000

    Minro fix for Insiderpages

--- Insiderpages/SignUp/client_script.rb
+++ Insiderpages/SignUp/client_script.rb
@@ -7,8 +7,10 @@
 @browser.text_field( :id, 'friend_password' ).set data[ 'password' ]
 @browser.text_field( :id, 'friend_password_confirmation' ).set data[ 'password' ]
 @browser.button( :value, 'join' ).click
+sleep 2
+Watir::Wait.until { @browser.text.include? "Please log into the email that you used to sign up for your account. You will receive a link from Insider Pages to confirm your email account."}
 
-if @browser.text.include? "Please log into the email that you used to sign up for your account. You will receive a link from Insider Pages to confirm your email account."
+
 
   puts( 'Signup complete, verify email.' )
 
@@ -19,4 +21,4 @@ if @browser.text.include? "Please log into the email that you used to sign up fo
   end
   true
 
-end
+

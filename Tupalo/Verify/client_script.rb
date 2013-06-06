08eee40bb5580fc2b6f45cda55db94423b6e9431
JoshWillis <blue74dot@gmail.com>
2013-06-06 17:18:23 +0000

    Quick fix for tupalo

--- Tupalo/Verify/client_script.rb
+++ Tupalo/Verify/client_script.rb
@@ -1,4 +1,9 @@
 goto_signin_page 
 process_tupalo_signin(data)
 RestClient.post "#{@host}/accounts.json?auth_token=#{@key}&business_id=#{@bid}", 'account[password]' => data['password'], 'model' => 'Tupalo'
+
+if @chained
+	self.start("Tupalo/AddListing")
+end
+
 true

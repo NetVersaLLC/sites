cdc98b51297bbcf9ad24646bff8676f3a2bc8354
amitr1983 <amitr1983@github.com>
2013-06-12 19:32:24 +0000

    Search for UScity

--- Uscity/SearchListing/client_script.rb
+++ Uscity/SearchListing/client_script.rb
@@ -1,6 +1,27 @@
-#Search business
+#Search for business
+citystate = data['city']+ ", " + data['state_short']
 
-if search_business(data)
-  puts "Business already exist"
-  true
+businessFound = {}
+
+businessfixed = data['business'].gsub(" ", "-")
+citystatefixed = citystate.gsub(", ","-")
+
+url = "http://uscity.net/#{citystatefixed}/#{businessfixed}"
+
+page = Nokogiri::HTML(RestClient.get(url)) 
+page.css("div.boxborder").each do |link|	
+  if link.text =~ /#{data['business']}/i
+    link.css('div.addressbox').map { |info| 
+      businessFound['listed_address'] = info.css("strong").text
+      businessFound['listed_phone'] = info.css("span.greentxt").text
+    }
+    link.css('h3 a').map { |info| 
+      businessFound['url'] = info['href']
+    }
+    businessFound['status'] = :claimed
+  else
+    businessFound['status']	= :unlisted
+  end
 end
+
+[true, businessFound]
--- Uscity/SearchListing/data_generator.rb
+++ Uscity/SearchListing/data_generator.rb
@@ -1,4 +1,5 @@
 data = {}
 data[ 'business' ]	= business.business_name
 data[ 'city' ]		= business.city
+data['state_short']	= business.state_name
 data

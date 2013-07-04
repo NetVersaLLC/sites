0d21b9dcc2ad0b8eae8e582dfeb98914d4bd6d97
amitr1983 <amitr1983@github.com>
2013-07-04 18:38:46 +0000

    Converting watir code into nokogiri:Getfave

--- Getfave/SearchListing/client_script.rb
+++ Getfave/SearchListing/client_script.rb
@@ -1,40 +1,20 @@
-@browser = Watir::Browser.new
-businessfixed        = data['business'].gsub(" ", "+")
-begin
- @browser.goto("https://www.getfave.com")
-
-sleep 2
-  @browser.link(:id => 'change-location').when_present.click
-  @browser.text_field(:id => 'g-text-field').set data['city'] + ", " + data['state_short']
-
-  raise Exception, 'Internal Server Error (500)' if @browser.text.include? 'Internal Server Error (500)'
-
-rescue Exception => e
-  puts(e.inspect)
-  if tries > 0
-    tries -= 1
-    retry
-  else
-    throw "Job failed due to server internal error."
-  end
+#Replace '&' with 'and'
+def replace_and(business)
+  return business.gsub("&","and")
 end
 
+url = "https://www.getfave.com/search?q=#{CGI.escape(data['business'])}&g=#{CGI.escape(data['location'])}"
+businessFound = {}
+page = Nokogiri::HTML(RestClient.get(url))
 
-  sleep 3
-
-  #@browser.text_field(:id => 'g-text-field').send_keys :enter
-  @browser.send_keys :enter
-sleep 5
-@browser.goto("https://www.getfave.com/search?q=#{businessfixed}")
-
-if @browser.text.include? "We couldn't find any matches."
-
+if page.text.include? "We couldn't find any matches."
   businessFound['status'] = :unlisted
 else
-  @browser.links(:id => /business_/).each do |resultLink|
-    if resultLink.span(:class => 'name').text =~ /#{data['business']}/i
-      businessFound['listed_address'] = resultLink.span(:class => 'address').text
-      businessFound['listed_url'] = resultLink.href
+  page.xpath("//div[@id='business-results']/a").each do |item|
+    if replace_and(item.css('span.name').text) =~ /#{replace_and(data['business'])}/i
+      businessFound['listed_name'] = item.css('span.name').text
+      businessFound['listed_address'] = item.css('span.address').text
+      businessFound['listed_url'] = item.attr('href')
 
       nok = Nokogiri::HTML(RestClient.get businessFound['listed_url'])
       if nok.css("a#claim").length > 0
@@ -42,15 +22,9 @@ else
       else
         businessFound['status'] = :claimed
       end
-
-
       break
     end
-
   end
-
 end
-
-at_exit do @browser.close end
 
 [true, businessFound]

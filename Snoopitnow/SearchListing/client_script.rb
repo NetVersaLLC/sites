87518d0317a79727b3bb134b03991e5ab88b3657
Dmitry Smirnov <dmitler@gmail.com>
2013-07-07 19:52:21 +0300

    AddListing fixed. SearchListing fixed.

--- Snoopitnow/SearchListing/client_script.rb
+++ Snoopitnow/SearchListing/client_script.rb
@@ -1,19 +1,27 @@
-url = "http://www.snoopitnow.com/index2.php?sobi2Search=#{data['businessfixed']}&searchphrase=any&field_area=&field_postcode=#{data['zip']}&option=com_sobi2&Itemid=26&no_html=1&sobi2Task=axSearch&sobiCid=0&SobiSearchPage=0&SobiCatSelected_0=0%20HTTP/1.1"
+businessFound = {}
 
-page = Nokogiri::HTML(RestClient.get(url))  
+url = "http://www.snoopitnow.com/Search/newest-first.html?condition=companyname&radiobutton=companyname&searchphrase=exact&searchword=#{data['businessfixed']}"
+
+page = Nokogiri::HTML(RestClient.get(url))
 
 thetable = page.css('table.sobi2Listing')
-if thetable.length == 0
-  businessFound = [:unlisted]
-else
-businessFound = [:unlisted]
-  thetable.search('table > tr').each do |row|
-    thelink = row.css('a')[0]
-    next if thelink == nil
 
-    if thelink.text =~ /#{data['business']}/
-      businessFound = [:listed, :unclaimed]
-      break
+if thetable.length.zero?
+  businessFound['status'] = :unlisted
+else
+  thetable.search('table > tr').each do |row|
+    business_name = row.css('h3')
+    next if business_name == nil
+
+    if business_name.text =~ /#{data['business']}/
+      businessFound['status'] = :listed
+
+      businessFound['listed_phone'] = thetable.xpath("//table//table/tr[3]/td[2]/div[1]").text.strip
+      nbsp = Nokogiri::HTML("&nbsp;").text
+      businessFound['listed_address'] = thetable.xpath("//table//table/tr[3]/td[2]").text.gsub(businessFound['listed_phone'], "").gsub(nbsp, " ").gsub("United States", "").gsub("\r\n", "").strip
+      businessFound['listed_categories'] = thetable.xpath("//table//table/tr[4]/td[2]").text.gsub("\r\n", "").strip
     end
   end
 end
+
+[true, businessFound]

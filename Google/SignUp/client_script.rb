6e84e4a269fd9d6c661884a0f729c5ddca6660c0
Jonathan Jeffus <jjeffus@gmail.com>
2013-07-08 11:48:19 -0700

    fixing google payload

--- Google/SignUp/client_script.rb
+++ Google/SignUp/client_script.rb
@@ -1,1 +1,8 @@
-system "gusto"
+ENV['fname'] = data['fname']
+ENV['lname'] = data['lname']
+ENV['useragent'] = data['useragent']
+ENV['recover_email'] = data['recover_email']
+ENV['bid'] = @bid
+ENV['key'] = @key
+ENV['ca_cert'] = "ca-bundle.crt"
+system "gusto.exe"

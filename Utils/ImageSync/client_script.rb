249d492aca7185d1c119156f71a328258d87ffe6
Jonathan Jeffus <jjeffus@gmail.com>
2013-07-07 18:19:56 -0700

    client updates

--- Utils/ImageSync/client_script.rb
+++ Utils/ImageSync/client_script.rb
@@ -3,3 +3,7 @@ images.each do |image|
   src = RestClient.get(image['url'])
   File.open("#{ENV['USERPROFILE']}\\citation\\#{$bid}\\images\\#{image}", "wb").write src
 end
+if data['logo'] != nil and  data['logo'] =~ /\.(?:png|jpe?g)$/
+  src = RestClient.get(data['logo'])
+  File.open("#{ENV['USERPROFILE']}\\citation\\#{$bid}\\logo.#{$1}", "wb").write src
+end
--- Utils/ImageSync/data_generator.rb
+++ Utils/ImageSync/data_generator.rb
@@ -1,1 +1,6 @@
 data['images'] = business.images
+business.images.each do |image|
+  if image.is_logo == true
+    data['logo'] = image.url()
+  end
+end

0604b8036fc82deb7347d3e9eadb54d11a38e5e5
Jonathan Jeffus <jonathan.jeffus@netversa.com>
2013-07-09 09:04:34 -0700

    updated imagesync

--- Utils/ImageSync/client_script.rb
+++ Utils/ImageSync/client_script.rb
@@ -1,8 +1,23 @@
 images = data['images']
 i = 0
 
-directory_name = "#{ENV['USERPROFILE']}\\citation\\#{$bid}\\images"
+root_dir = "#{ENV['USERPROFILE']}\\citation\\#{$bid}"
+directory_name = "#{root_dir}\\images"
 Dir.mkdir(directory_name) unless File.exists?(directory_name)
+
+Dir.open(directory_name).each do |file|
+  next if file =~ /^\./
+  FileUtils.rm "#{directory_name}\\#{file}"
+end
+
+logo = self.logo
+if File.exists? logo
+  FileUtils.rm logo
+end
+if data['logo'] and data['logo'] =~ /\.(png|jpe?g)/i
+  src = RestClient.get(data['logo'])
+  File.open("#{ENV['USERPROFILE']}\\citation\\#{$bid}\\logo.#{$1}", "wb").write src
+end
 
 images.each do |image|
   i = i + 1

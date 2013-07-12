51bdbcc34470fc968b6711ec5ab21713d3552ed6
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-07-12 21:34:28 +0000

    Zippro misc. edits

--- Zippro/Verify/client_script.rb
+++ Zippro/Verify/client_script.rb
@@ -2,7 +2,7 @@
 sleep(10) #wait for the stupid auto redirect
 if @browser.text.include? "Profile Manager"
 	if @chained
-		self.start("Zippro/Notify")
+		self.start("Zippro/VerifyPhone")
 	end
 	true
 

2d2bcb158d2514732f94835a68df7b596308d3aa
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-07-17 23:34:22 +0000

    Rescue for AddListing

--- Ebusinesspage/AddListing/client_script.rb
+++ Ebusinesspage/AddListing/client_script.rb
@@ -2,6 +2,8 @@ sign_in(data)
 
 @browser.goto( 'http://ebusinesspages.com/AddCompany.aspx' )
 
+retries = 5
+begin
 @browser.text_field( :id => 'stitle').set data['business']
 @browser.text_field( :id => 'sstreet').set data['addressComb']
 @browser.text_field( :id => 'szip').set data['zip']
@@ -10,12 +12,20 @@ sign_in(data)
 @browser.text_field( :id => 'semail').set data['email']
 @browser.text_field( :id => 'surl').set data['website']
 
+if @browser.text_field( :id => 'sCatKey').exists?
 @browser.text_field( :id => 'sCatKey').set data['category1']
 @browser.button( :id => 'CatButton').click
 sleep(2)
 @browser.link( :text => /#{data['category1']}/i).click
 sleep(1)
+end
 @browser.button( :id => 'CoButton').click
+rescue
+	if retries > 0 then
+	retry
+	retries -= 1
+	end
+end
 
 if @browser.text.include? "Update successful!"
 	true

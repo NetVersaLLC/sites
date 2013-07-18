eb6292707be3c1a4ba4950e25df39f22744bfd41
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-07-18 20:23:31 +0000

    Bing fixes

--- Bing/AdditionalDetails/client_script.rb
+++ Bing/AdditionalDetails/client_script.rb
@@ -1,13 +1,13 @@
 sign_in_business( data )
 sleep 2
-Watir::Wait.until { @browser.text.include? "Manage Your Listings" }
+Watir::Wait.until { @browser.text.include? "All Businesses" }
 
 @browser.link(:text => 'Edit').click
 
 sleep 2
 retries = 3
 begin
-	@browser.h4(:text => 'Additional Business Details').when_present.click
+	@browser.h5(:text => 'Additional Business Details').when_present.click
 	sleep 1 #Animation length
 	@browser.text_field(:name => 'AdditionalBusinessInfo.OpHourDetail.AdditionalInformation').set data['hours']
 	@browser.text_field(:name => 'AdditionalBusinessInfo.Description').set data['description']
@@ -23,7 +23,7 @@ end
 
 retries = 3
 begin
-	@browser.h4(:text => "Other Contact Information").click
+	@browser.h5(:text => "Other Contact Information").click
 	sleep 2
 	if data['mobile_appears']
 		@browser.text_field(:name => 'AdditionalBusinessInfo.MobilePhoneNumber').set data['mobile']
@@ -41,7 +41,7 @@ end
 
 retries = 3
 begin
-	@browser.h4(:text => "General Information").click
+	@browser.h5(:text => "General Information").click
 	sleep 1
 
 	data['payments'].each do |pay|
@@ -60,7 +60,7 @@ end
 @browser.button(:id => 'submitBusiness').click
 
 sleep 2
-Watir::Wait.until { @browser.text.include? "Manage Your Listings" }
+Watir::Wait.until { @browser.text.include? "All Businesses" }
 
 if @chained
 	self.start("Bing/VerifyMail")

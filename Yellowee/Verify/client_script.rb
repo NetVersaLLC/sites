2c93b7ed607401ce558356ba7a9306764b363f81
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-07-23 18:15:53 +0000

    Verify delay added

--- Yellowee/Verify/client_script.rb
+++ Yellowee/Verify/client_script.rb
@@ -1,7 +1,12 @@
+if data['url'].nil? then
+	if @chained
+		self.start("Yellowee/Verify", 1440)
+	end
+	true
+else
 @browser.goto(data['url'])
-
+end
 if @browser.text.include? "Invalid activation key."
-
 	throw("There was a problem activating the account")
 else
 	if @chained

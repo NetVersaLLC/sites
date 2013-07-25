101e4bb2d2cd31d582e2c6b2460483a557357bc3
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-07-25 23:29:38 +0000

    Script rewrite

--- Gomylocal/SearchListing/client_script.rb
+++ Gomylocal/SearchListing/client_script.rb
@@ -1,27 +1,24 @@
-#Search business
-def search_business(data)
-  @browser.text_field(:id => 'search_bar_category').set data['business']
-  @browser.text_field(:id => 'search_bar_zip').set data['zip']
-  @browser.button(:name =>'imageField').click
-  # Check for business
-  $businessFound = [:unlisted]
-  page = Nokogiri::HTML(@browser.html)
-  page.css('a').each do |link|
-    business = link.text.strip
-    if business == data['business']
-      @browser.link(:text=>/#{data[ 'business' ]}/).click
-      if @browser.text.include? 'Claim Business'
-      # if @browser.link(:text => 'Claim Business').exist?
-        $businessFound = [:listed,:unclaimed]
-      else
-        $businessFound = [:listed,:claimed]
-      end
-    end
-  end
-  return [true, $businessFound]
-end
+#Written by Kazyyk, 7/25/13
 
-#main steps
-url = 'http://www.gomylocal.com/'
-@browser.goto url
-search_business data
+data['businessfixed'] = data['business'].gsub(" ","+").gsub(",","")
+
+page = Nokogiri::HTML(open("http://www.gomylocal.com/listings/" + data['zip'] + "-" + data['businessfixed']))
+if page.at_css("a.links_12pt_bold").text.include? data['business'] then
+  if not page.at_xpath("//a[text()='Claim Business']").nil? then
+    puts("Business is not claimed")
+    businessFound['status'] = :unclaimed
+  else
+    puts("Business is claimed")
+    businessFound['status'] = :claimed
+  end
+  businessFound['listed_url'] = page.at_css("a.links_12pt_bold")["href"]
+  puts("Listed Url: " + businessFound['listed_url'])
+  page = Nokogiri::HTML(open(page.at_css("a.links_12pt_bold")["href"]))
+  businessFound['listed_address'] = page.css("span.span_new")[0].text.gsub(/(&nbsp;)+/, "") + page.css("span.span_new")[1].text.gsub(/(&nbsp;)+/, "")
+  puts("Listed Address: " + businessFound['listed_address'])
+  businessFound['listed_phone'] = page.css("span.phonenumber").text.gsub(/(&nbsp;)+/, "")
+  puts("Listed Phone: " + businessFound['listed_phone'])
+else
+  puts("Business is unlisted")
+  businessFound['status'] =  :unlisted
+end

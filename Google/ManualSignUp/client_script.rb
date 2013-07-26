4de3ffe089853ad1030b6d39314d859ccefe0e89
Jonathan Jeffus <jjeffus@gmail.com>
2013-07-26 08:59:47 -0700

    adding manual sign up

---
+++ Google/ManualSignUp/client_script.rb
@@ -0,0 +1,5 @@
+output = `glogin.exe`
+output.gsub!(/^\s*/, '')
+output.gsub!(/\s*$/, '')
+email, password = *output.split(/\t/)
+self.save_account('Google',{:email => email, :password => password})

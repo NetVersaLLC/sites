b0a1dccde698e5dbbb8fe73d21c4de1742e25af9
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-07-26 01:00:39 +0000

    SearchListing fix

--- Justclicklocal/SearchListing/client_script.rb
+++ Justclicklocal/SearchListing/client_script.rb
@@ -3,19 +3,21 @@ data['cityfixed'] = data['city'].gsub(" ","-")
 data['businessfixed'] = data['business'].gsub(" ","-").gsub(" & ","").gsub(",","")
 
 url = "http://www.justclicklocal.com/citydir/#{data[ 'cityfixed' ]}-#{data[ 'state' ]}--#{data[ 'businessfixed' ]}.html"
-puts(url)
+#puts(url)
 page = Nokogiri::HTML(RestClient.get(url)) 
 if page.css("ol.initial-results") #Do results even exist?
   result = page.at_xpath('//*[@id="r"]/div[2]/div[1]/ol[1]/li[1]/div[2]/a').text #Grab first result
   if result == data[ 'business' ] #Match results, otherwise assume unlisted
-    businessFound['status'] = :listed
-    puts("Business is listed")
+    businessFound['status'] = :claimed
+    #puts("Business is listed")
+    businessFound['listed_name'] = result
+    #puts("Listed Name: " + result)
     businessFound['listed_address'] = page.at_css("span.address").text
-    puts("Listed Address: " + businessFound['listed_address'])
+    #puts("Listed Address: " + businessFound['listed_address'])
     businessFound['listed_phone'] = page.at_css("span.phone-num").text
-    puts("Listed Phone: " + businessFound['listed_phone'])
+    #puts("Listed Phone: " + businessFound['listed_phone'])
   else
-    puts("Business is unlisted")
+    #puts("Business is unlisted")
     businessFound['status'] = :unlisted
 end
 

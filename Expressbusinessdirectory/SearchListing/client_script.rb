27f301a3bb07a420dafa639294256411c84b19cf
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-07-26 00:55:11 +0000

    SearchListing phone update

--- Expressbusinessdirectory/SearchListing/client_script.rb
+++ Expressbusinessdirectory/SearchListing/client_script.rb
@@ -6,16 +6,23 @@ end
 data[ 'businessfixed' ] = format_business(data['business'])
 
 url = "http://www.expressbusinessdirectory.com/businesses/#{data[ 'businessfixed' ] }/"
+#puts(url)
 
 page = Nokogiri::HTML(RestClient.get(url))
 businessFound = {}
 businessFound['status'] = :unlisted
 page.css("a#ctl00_ContentPlaceHolder1_dlResults_ctl00_hypBusiness").each do |resultLink|
   if resultLink.text.gsub('&', 'and') =~ /#{data['business'].gsub('&', 'and')}/i
+    resultContent = Nokogiri::HTML(open(resultLink["href"]))
     businessFound['status'] = :claimed
     businessFound['listed_name'] = resultLink.text # Return business name given on webpage
+    #puts("Listed Name: " + businessFound['listed_name'])
     businessFound['listed_url'] = resultLink.attr("href")
-    businessFound['listed_address']	= page.css("span#ctl00_ContentPlaceHolder1_dlResults_ctl00_lblAddress").text
+    #puts("Listed Url: " + businessFound['listed_url'])
+    businessFound['listed_address'] = page.css("span#ctl00_ContentPlaceHolder1_dlResults_ctl00_lblAddress").text
+    #puts("Listed Address: " + businessFound['listed_address'])
+    businessFound['listed_phone'] = resultContent.xpath('//*[@id="content-left"]/text()[6]').to_s.gsub("phone:","").gsub(" ","")
+    #puts("Listed Phone: " + businessFound['listed_phone'])
   end
 end
 

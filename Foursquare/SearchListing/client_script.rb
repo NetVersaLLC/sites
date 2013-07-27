745df15f847de9624860de2c07d5d19ecebe0b18
JoshWillis <blue74dot@gmail.com>
2013-07-27 19:14:59 +0000

    Foursquare Searchlisting overhaul

--- Foursquare/SearchListing/client_script.rb
+++ Foursquare/SearchListing/client_script.rb
@@ -1,3 +1,36 @@
-search_for_business( data )
+businessFixed = data['business'].gsub(" ","%20")
+businessFound = {}
+url = "http://foursquare.com/explore?near=#{data['zip']}&q=#{businessFixed}"
+puts (url)
+page = Nokogiri::HTML(RestClient.get(url)) 
 
-sleep(30)
+#puts page
+found = false
+page.css("div.venueBlock").each do |venue|	
+	if venue.css("div.venueName").text =~ /#{data['business']}/
+		found = true
+		businessFound['listed_name'] 	= venue.css("div.venueName").text 
+		businessFound['listed_url'] 	= "https://foursquare.com"+venue.css("div.venueName a")[0].attr("href")
+
+		puts businessFound['listed_url']
+		subpage = Nokogiri::HTML(RestClient.get(businessFound['listed_url'])) 
+		businessFound['listed_address'] = subpage.css("span.street-address").text + ", " + subpage.css("span.locality").text + subpage.css("span.region").text + ", " +subpage.css("span.postal-code").text
+		businessFound['listed_phone']	= subpage.css("span.tel").text
+		puts subpage.search "[text()*='Claim it now.']"
+		if subpage.search "[text()*='Claim it now.']"
+			businessFound['status'] = :claimed
+			#currently no was to tell if claimed or unclaimed
+		else
+			businessFound['status'] = :claimed
+		end
+		break
+	end
+end
+
+	if not found
+		businessFound['status'] = :unlisted
+	end
+
+
+[true,businessFound]
+

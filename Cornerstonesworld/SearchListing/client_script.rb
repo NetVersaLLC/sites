4b26549a727991b9c8582cf1bdd84217df338841
JoshWillis <blue74dot@gmail.com>
2013-07-28 00:38:32 +0000

    Fixes for Cornerstonesworld/SearchListing

--- Cornerstonesworld/SearchListing/client_script.rb
+++ Cornerstonesworld/SearchListing/client_script.rb
@@ -13,13 +13,14 @@ if item.nil?
   businessFound['status'] = :unlisted
   else
   if replace_and(item.css("span.titlesmalldblue").text.gsub(/\t+|\n+/, '') .strip) =~ /#{replace_and(data['business'])}/i
-    businessFound['status'] = :listed
+    businessFound['status'] = :claimed
     businessFound['listed_name'] = item.css("span.titlesmalldblue").text # Return business name given on webpage
     businessFound['listed_address'] = item.search("//p")[1].text.gsub(/\t+/, '') .strip
-    businessFound['listed_phone'] = item.search("//p")[2].text.gsub(/[\x80-\xff]|\n+|\t+|[a-zA-Z]|\:/,'')
+    businessFound['listed_phone'] = item.search("//p")[2].text[ /\b[\s()\d-]{6,}\d\b/ ] #.gsub(/[\x80-\xff]|\n+|\t+|[a-zA-Z]|\:/,'')
     businessFound['listed_url'] = url
   end
   end
 end
 
+#puts businessFound
 [true, businessFound]

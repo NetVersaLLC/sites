d478b5fc6109380138896d01cf2588511df0e53b
JoshWillis <blue74dot@gmail.com>
2013-07-29 18:11:08 +0000

    Zippro SearchListing fixed

--- Zippro/SearchListing/data_generator.rb
+++
@@ -1,5 +0,0 @@
-data = {}
-data[ 'business' ]          = business['business']
-data[ 'businessfixed' ]          = business['business'].gsub(" ", "+").gsub("-","+")
-data['zip']   = business['zip']
-data

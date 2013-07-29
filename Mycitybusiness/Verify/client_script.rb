46e6e40ab1218102afa875a866efb7be2b8416c1
JoshWillis <blue74dot@gmail.com>
2013-07-29 22:22:23 +0000

    Verify 24hr delay

--- Mycitybusiness/Verify/client_script.rb
+++ Mycitybusiness/Verify/client_script.rb
@@ -1,5 +1,11 @@
-@browser.goto(data['url'])
-
-if @browser.text.include? "Thank you for adding your business to mycityBusiness.net."
-  true
+if data['url'].nil? then
+	if @chained
+		self.start("Mycitybusiness/Verify", 1440)
+		true
+	end
+else
+	@browser.goto(data['url'])
+	if @browser.text.include? "Thank you for adding your business to mycityBusiness.net."
+	  true
+	end
 end

73676d107987ecb1c64afcdd56b543dad4031999
amitr1983 <amitr1983@github.com>
2013-07-31 18:51:51 +0000

    New Searchlisting for manta

--- Manta/SearchListing/client_script.rb
+++ Manta/SearchListing/client_script.rb
@@ -1,37 +1,32 @@
-@browser.goto('http://www.manta.com/')
+  data['business_location'] =  data[ 'business' ] +', '+data[ 'city' ] +' '+ data[ 'state' ] 
 
-@browser.text_field( :name => 'search').set data[ 'query' ]
-sleep(2)
-@browser.button( :class => 'button').click
-sleep(4)
+  #replace '&' with 'and'
+  def replace_and(business)
+    return business.gsub("&","and")
+  end
 
-begin 
-  @browser.image( :id => 'skip').exists?
-  @browser.image( :id => 'skip').click
-rescue Timeout::Error
+  # Main steps
+  @browser.goto( "http://www.manta.com" )
+  @browser.text_field( :id => 'search-home-lo' ).set data['business_location']
+  @browser.form(:id=> 'search-home-lo-submit').button.click
+  @browser.wait_until {@browser.div(:class=> 'results-area').exist?}
+  
+   businessFound = {}
+   businessFound['status'] = :unlisted
+   page = Nokogiri::HTML(@browser.html)
+   page.css('li > div.result-box').each do |item|
 
-end
-
-
-sleep(5)
-
-businessFound = []
-if @browser.text.include? "We couldn't find any companies that matched your selections"
-    businessFound = [:unlisted]
-else
-    begin
-      @browser.link( :text => data['business']).exists?
-      @browser.link( :text => /#{data['business']}/).click
-        begin 
-            @browser.div( :class => 'aside own_this_business').exists?
-            businessFound = [:listed, :unclaimed]
-        rescue Timeout::Error
-            businessFound = [:listed, :claimed]
-        end
-    rescue Timeout::Error
-    
-    end
-     
-end
-
-return true, businessFound
+  if replace_and(item.at_css("h2").text) =~ /#{replace_and(data['business'])}/
+      businessFound['listed_name'] = item.at_css("h2").text.gsub(/\t+|\r\n+/,'').strip # Return business name given on webpage
+      businessFound['listed_url'] = item.at_css("a").attr('href')
+      businessFound['listed_address'] = item.xpath("//div[@itemprop='address']")[0].text.gsub(/\t+|\r\n+/,'').strip
+      subpage = Nokogiri::HTML(RestClient.get businessFound['listed_url'])
+      if subpage.css('div.claimable_link').text.include?('Claim This Profile')
+      businessFound['status'] = :listed
+      else
+      businessFound['status'] = :claimed
+      end
+      break
+  end
+  end
+  [true, businessFound]

e4b9ef1ac529ef49f49cd53a1320224ab7daca33
Coin Age <coinage7@gmail.com>
2013-07-31 21:18:27 +0000

    Tupalo SignUp data_generator fix

--- Tupalo/SignUp/data_generator.rb
+++ Tupalo/SignUp/data_generator.rb
@@ -1,6 +1,6 @@
 data = {}
 seedString 					= rand( 1000 ).to_s()
 data[ 'userName' ] 			= (business.contact_first_name + business.contact_last_name + seedString).to_s.gsub(/\s+/, '')
-data['email'] 				= "yetanotheraccount07@outlook.com"#business.bings.first.email
+data['email'] 				= business.bings.first.email
 data['password']			= business.bings.first.password
 data

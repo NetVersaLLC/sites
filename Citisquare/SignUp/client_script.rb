ba2129aa14239d49181caee27ff12ad52d6f0b98
Coin Age <coinage7@gmail.com>
2013-08-14 15:40:08 +0000

    Adding sleep to Citisquare\SignUp

--- Citisquare/SignUp/client_script.rb
+++ Citisquare/SignUp/client_script.rb
@@ -1,5 +1,6 @@
 def sign_up(data)
   @browser.link(:text => 'Sign Up Now').click
+  sleep 2
   @browser.text_field(:name => 'name').when_present.set data['username']
   @browser.text_field(:name => 'mail').set data['email'] 
   @browser.text_field(:name => 'conf_mail').set data['email'] 
@@ -9,6 +10,7 @@ def sign_up(data)
   @browser.text_field(:name => 'last_name').set data['last_name'] 
   @browser.text_field(:name => 'zip').set data['zip'] 
   @browser.button(:value => 'Register').click
+  sleep 2
   RestClient.post "#{@host}/accounts.json?auth_token=#{@key}&business_id=#{@bid}", 'account[email]' => data['email'], 'account[password]' => data['password'], 'model' => 'Citisquare'
 end  
   
@@ -16,6 +18,8 @@ end
 # Launch url
 url = 'http://my.citysquares.com/search'
 @browser.goto url
+
+sleep 2
 
 @browser.text_field(:name => 'b_standardname').set data['business'] 
 @browser.text_field(:name => 'b_zip').set data['zip'] 
@@ -26,13 +30,18 @@ if matching_result data
   claim_business data
 else  
   puts "Adding New business listing"
+  sleep 2
   @browser.link(:text => 'add it here!').when_present.click
   @browser.link(:href => /free/).click
 
+  sleep 2
   login data 
+
+  sleep 2
 	fill_listing_form data
 
   @browser.button(:value => 'Add Business').click
+  sleep 2
   @confirmation = @browser.div(:id => 'landingWelcome')
   @confirmation_msg = "Welcome to your business dashboard #{data['first_name']}"
   

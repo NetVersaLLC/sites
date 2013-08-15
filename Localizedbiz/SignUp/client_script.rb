b3850c5780aed48f936c5c99366e5fcdb4820960
Coin Age <coinage7@gmail.com>
2013-08-15 18:19:42 +0000

    localizedbiz signup and verify improvements.

--- Localizedbiz/SignUp/client_script.rb
+++ Localizedbiz/SignUp/client_script.rb
@@ -1,5 +1,5 @@
 @browser.goto("http://localizedbiz.com/login/register.php")
-30.times{break if (begin @browser.text_field(:name, "email").present? rescue Selenium::WebDriver::Error::NoSuchElementError end) == true; sleep 1}
+30.times{ break if @browser.status == "Done"; sleep 1}
 @browser.text_field( :id => 'username').set data['username']
 @browser.text_field( :id => 'password').set data['password']
 @browser.text_field( :id => 'password_confirmed').set data['password']
@@ -7,10 +7,13 @@
 
 @browser.button( :name => 'register').click
 
-30.times{break if (begin @browser.text.include?("Account registered. Please check your email for details on how to activate it.") rescue Selenium::WebDriver::Error::NoSuchElementError end) == true; sleep 1}
-
-self.save_account("Localizedbiz", {:username => data['username'], :password => data['password']})
-if @chained
-	self.start("Localizedbiz/Verify")
+30.times{ break if @browser.status == "Done"; sleep 1}
+if @browser.text.include? "The email you used is associated with another user. Please try again or use the \"forgot password\" feature!"
+	puts "Email already registered."	
+else
+	self.save_account("Localizedbiz", {:username => data['username'], :password => data['password']})
+	if @chained
+		self.start("Localizedbiz/Verify")
+	end	
 end
 true

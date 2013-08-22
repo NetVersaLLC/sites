aeddde5062e99b2daef3cc2f0b41d27f0b8957e6
amitr1983 <amitr1983@github.com>
2013-08-22 21:58:39 +0530

    Fix for Bug# 55325574:Yellowassistance

--- Yellowassistance/SearchListing/client_script.rb
+++ Yellowassistance/SearchListing/client_script.rb
@@ -1,6 +1,15 @@
 
-data[ 'citystate' ]          = data[ 'city' ] + ", " + data[ 'state' ]
-data[ 'businessfixed' ]     = data['business'].gsub(" ", "%20")
+def business_clean(data)
+  business_clean= data['business']
+  if business_clean.include?(' -')
+    business = data['business'].split(' -')
+    business_clean = business[0]
+  end
+  return business_clean
+end
+
+data[ 'citystate' ] = data[ 'city' ] + ", " + data[ 'state' ]
+data[ 'businessfixed' ] = business_clean(data).gsub(" ", "%20")
 
 url = "http://www.yellowassistance.com/frmBusResults.aspx?nas=a&nam=#{data['businessfixed']}&cas=r&cat=#{data['businessfixed']}&mis=a&mil=&zis=a&zip=#{data['zip']}&cis=r&cit=#{data['city']}&sts=r&sta=#{data['state']}&typ=bus&set=a&myl=&slat=&slon="
 puts url

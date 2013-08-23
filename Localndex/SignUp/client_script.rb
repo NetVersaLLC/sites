a0ee4a3ab72ad49e3390b6069d83178c8a25152e
Coin Age <coinage7@gmail.com>
2013-08-23 11:25:47 -0700

    rescued registration

--- Localndex/SignUp/client_script.rb
+++ Localndex/SignUp/client_script.rb
@@ -1,15 +1,30 @@
+def user_registration(data)
 @browser.goto( 'http://www.localndex.com/register.aspx' )
-#Fill user details.
 30.times{ break if @browser.status == "Done"; sleep 1}
 @browser.text_field(:id, 'ctl00_MainContentPlaceHolder_txtUserName').set data ['username']
 @browser.text_field(:id, 'ctl00_MainContentPlaceHolder_txtUserEmail').set data ['email']
 @browser.text_field(:id, 'ctl00_MainContentPlaceHolder_txtUserPass1').set data ['password']
 @browser.text_field(:id, 'ctl00_MainContentPlaceHolder_txtUserPass2').set data ['password']
-
-enter_captcha(data)	#Captcha being entered here!
-
-30.times{ break if @browser.status == "Done"; sleep 1}
+rescue => e
+  unless @retries == 0
+    puts "Error caught in initial registration: #{e.inspect}"
+    puts "Retrying in two seconds. #{@retries} attempts remaining."
+    sleep 2
+    @retries -= 1
+    retry
+  else
+    raise "Error in initial registration could not be resolved. Error: #{e.inspect}"
+  end
+end
+def chain_payload
 if @chained
-	self.start("Kudzu/CreateListing")
+	self.start("Localndex/Verify")
 end
 true
+end
+# Global Retry Count, affects all rescues.
+@retries = 5
+user_registration(data)											#Sign_up form filling.
+enter_captcha(data) 										#Entering the captcha.
+30.times{ break if @browser.status == "Done"; sleep 1}		#Wait for page load.
+chain_payload 												#Chain the subsequent payload.

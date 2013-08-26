5a5cc9ab49e6685b0d27f2fa23caa22fb1ceeb72
Coin Age <coinage7@gmail.com>
2013-08-26 11:21:31 -0700

    Bing/Create rule wait improvements.

--- Bing/CreateRule/client_script.rb
+++ Bing/CreateRule/client_script.rb
@@ -1,22 +1,23 @@
 sign_in( data )
 @browser.goto( 'mail.live.com/mail/options.aspx' )
 
+30.times { break if (begin @browser.link(:id => "iShowSkip").exists? or @browser.frame( :id, 'appFrame').exists? rescue Selenium::WebDriver::Error::NoSuchElementError end) == true; sleep 1 }
 
-sleep 2
-Watir::Wait.until{@browser.link(:id => "iShowSkip").exists? or @browser.frame( :id, 'appFrame').exists?}
 if @browser.link(:id => "iShowSkip").exists?
 	@browser.link(:id => "iShowSkip").click
-	sleep 2
-	Watir::Wait.until(360) { @browser.text.include? "Account summary" }
+
+	30.times { break if (begin @browser.text.include? "Account summary" rescue Selenium::WebDriver::Error::NoSuchElementError end) == true; sleep 1 }
+	
 	@browser.goto( 'mail.live.com/mail/options.aspx' )	
 
-	sleep 2
-	Watir::Wait.until{@browser.frame( :id, 'appFrame').exists?}	
+	30.times { break if (begin @browser.frame( :id, 'appFrame').exists? rescue Selenium::WebDriver::Error::NoSuchElementError end) == true; sleep 1 }
+	
 end
 
-
 @opt_frame = @browser.frame( :id, 'appFrame')
+30.times { break if (begin @opt_frame.link( :text, /Rules for sorting new messages/i).present? rescue Selenium::WebDriver::Error::NoSuchElementError end) == true; sleep 1 }
 @opt_frame.link( :text, /Rules for sorting new messages/i).when_present.click
+30.times { break if (begin @opt_frame.button( :id, 'NewFilter').present? rescue Selenium::WebDriver::Error::NoSuchElementError end) == true; sleep 1 }
 @opt_frame.button( :id, 'NewFilter').when_present.click
 
 @opt_frame.select_list( :id, 'VerbDropDown').select "contains"

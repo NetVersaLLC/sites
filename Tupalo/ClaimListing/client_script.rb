a48101b1d5895164fd671594f0183151283f560d
Coin Age <coinage7@gmail.com>
2013-08-27 09:51:48 -0700

    minor fixes

--- Tupalo/ClaimListing/client_script.rb
+++ Tupalo/ClaimListing/client_script.rb
@@ -1,33 +1,28 @@
-url="http://tupalo.com/en/accounts/sign_in"
-@browser.goto(url)
-
-sleep 2
-Watir::Wait::until {@browser.text.include? "Login"}
+@browser.goto("http://tupalo.com/en/accounts/sign_in")
+30.times{ break if @browser.status == "Done"; sleep 1}
 
 @browser.text_field(:id => "account_email").set data ['email']
 @browser.text_field(:id => "account_password").set data['password']
 @browser.button(:id => "spot_submit").click
 
-sleep 2
-Watir::Wait::until {@browser.text.include? "My Favorites"}
+30.times{ break if @browser.status == "Done"; sleep 1}
 
 url=@browser.url+"?only=discovered_spots"
 @browser.goto(url)
 
-sleep 2
-Watir::Wait::until {@browser.text.include? "My Discoveries"}
+30.times{ break if @browser.status == "Done"; sleep 1}
 
 @browser.span(:class => "index" ).click
 
-sleep 2
-Watir::Wait::until {@browser.text.include? "Category:"}
+30.times{ break if @browser.status == "Done"; sleep 1}
 
 url=@browser.a(:text => /Claim your business listing/).href
 @browser.goto(url)
 
-sleep 2
-Watir::Wait::until {@browser.text.include? "Promote your business"}
+30.times{ break if @browser.status == "Done"; sleep 1}
 
 @browser.a(:class => /button green color/).click
-sleep 2
+
+30.times{ break if @browser.status == "Done"; sleep 1}
+
 true

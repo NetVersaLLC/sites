55ec36c96c4c180afe2a6af3d8799025cd7e1b38
Coin Age <coinage7@gmail.com>
2013-08-28 12:37:09 -0700

    opening hours improve.

--- Tupalo/AddListing/client_script.rb
+++ Tupalo/AddListing/client_script.rb
@@ -24,15 +24,20 @@ if @browser.select_list(:id => "sublevel_category")
 end
 
 @browser.text_field(:id => "spot_street").set data['address']#Enter the Business address
-
 @browser.text_field(:id => "spot_phone").set data['phone']#Local phone number
 @browser.text_field(:id => "spot_website").set data['website']#Set web site
 
 #Enter the working hours 
 days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
-
+count=0
 days.each do |day|
-	if data["#{day}"] == true		
+count+=1
+	if data["#{day}"] == true	
+		if count == 7
+			@browser.select_list(:name => "new_opening_hour_day").option(:value => "0").select
+		else
+			@browser.select_list(:name => "new_opening_hour_day").option(:value => "#{count}").select
+		end
 		@browser.select_list(:name => "new_opening_hour_hours_begin").select data["#{day}_open"].gsub(/[AP]M/,"")
 		closehours = data["#{day}_close"].gsub(/[AP]M/,"").split(":")[0].to_i
 		if 	data["#{day}_close"] =~ /PM$/i #Convert closing hours to 24 hours format
@@ -42,13 +47,13 @@ days.each do |day|
 		close = closehours.to_s + ":" + closemin		
 		@browser.select_list(:name => "new_opening_hour_hours_end").select close
 		@browser.span(:class => /button small/).click		
-	end	
+	end
 end
 
 @browser.text_field(:id => 'spot_city_and_country').clear
 @browser.text_field(:id => 'spot_city_and_country').fire_event("onclick")
 #data=data['city']+", "+data['state'] #include this line to be more closer to the actual location.
-@browser.text_field(:id => 'spot_city_and_country').send_keys data['city']#Select the city
+@browser.text_field(:id => 'spot_city_and_country').send_keys data['citystatecountry']#Select the city
 sleep 10 #Let the list populate.
 @browser.send_keys :tab
 @browser.send_keys :enter

58d453b34a67df0a008f3f9b06f9203c79563640
Jonathan Jeffus <jjeffus@gmail.com>
2013-08-28 16:50:50 -0700

    fixing ebusinesspage

--- Ebusinesspage/SearchListing/client_script.rb
+++ Ebusinesspage/SearchListing/client_script.rb
@@ -1,30 +1,13 @@
-#require 'rest-client'
-#require 'nokogiri'
-
-#data = {}
-#data['business']  = "Lets Fly Cheaper"
-#data['phone'] = "8002400461"
-
-p1 = data['phone'][0,3]
-p2 = data['phone'][3,3]
-p3 = data['phone'][6,4]
-
-url = "http://ebusinesspages.com/(#{p1})#{p2}-#{p3}.tel"
-
-
-#url = "http://ebusinesspages.com/search.aspx?co=#{data['businessfixed']}&loc=#{data['cityfixed']},%20+#{data['state']}"
-puts url
-
-page = Nokogiri::HTML(RestClient.get(url))
-
-thelist = page.css("div.listCompany a")
 businessFound = {}
 businessFound['status'] = :unlisted
+
+if data['phone'] =~ /\(?(\d\d\d)\)?[-.]?\s*(\d\d\d)[-.]?\s*(\d\d\d\d)/
+url = "http://ebusinesspages.com/(#{$1})#{$2}-#{$3}.tel"
+page = Nokogiri::HTML(RestClient.get(url))
+thelist = page.css("div.listCompany a")
 thelist.each do |item|
   next if not item.text =~ /#{data['business']}/i  
-  puts(item.text)
   thelink = "http://ebusinesspages.com/"+item['href']
-  puts(thelink)
   subpage = Nokogiri::HTML(RestClient.get(thelink))  
   
   businessFound['listed_address'] = subpage.css("span[@itemprop='street-address']").text + ", " + subpage.css("span[@itemprop='locality']").text + ", " + subpage.css("span[@itemprop='region']").text + ", " + subpage.css("span[@itemprop='postalCode']").text 
@@ -36,5 +19,5 @@ thelist.each do |item|
   break
   
 end
-#puts businessFound
+end
 [true, businessFound]

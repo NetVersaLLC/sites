2b85674b2d31e9dcb7d1b8e2e62ed485054b5ad8
nixoid <andrey.zamovskiy@skykillers.com>
2013-10-01 21:43:16 -0700

    fixed foursquare grabber

--- Foursquare/SearchListing/client_script.rb
+++ Foursquare/SearchListing/client_script.rb
@@ -2,18 +2,19 @@ businessFixed = data['business'].gsub(" ","%20")
 businessFound = {}
 url = "http://foursquare.com/explore?near=#{data['zip']}&q=#{businessFixed}"
 puts (url)
-page = Nokogiri::HTML(RestClient.get(url)) 
+page = Nokogiri::HTML(RestClient.get(url))
+
 
 #puts page
 found = false
-page.css("div.venueBlock").each do |venue|	
+page.css("div.venueBlock").each do |venue|
 	if venue.css("div.venueName").text =~ /#{data['business']}/
 		found = true
-		businessFound['listed_name'] 	= venue.css("div.venueName").text 
-		businessFound['listed_url'] 	= "https://foursquare.com"+venue.css("div.venueName a")[0].attr("href")
+		businessFound['listed_name'] 	= venue.css("div.venueName").text
+		businessFound['listed_url'] 	= venue.css("div.venueName a")[0].attr("href")
 
 		puts businessFound['listed_url']
-		subpage = Nokogiri::HTML(RestClient.get(businessFound['listed_url'])) 
+		subpage = Nokogiri::HTML(RestClient.get(businessFound['listed_url']))
 		businessFound['listed_address'] = subpage.css("span.street-address").text + ", " + subpage.css("span.locality").text + subpage.css("span.region").text + ", " +subpage.css("span.postal-code").text
 		businessFound['listed_phone']	= subpage.css("span.tel").text
 		puts subpage.search "[text()*='Claim it now.']"

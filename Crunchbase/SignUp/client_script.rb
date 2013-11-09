691f18a9c5e4fb11c7008b6b9165158a6bf939ba
David <davidcollazo@gmail.com>
2013-11-08 16:14:52 -0800

    Removed placeholders from Crunchbase scripts

--- Crunchbase/SignUp/client_script.rb
+++ Crunchbase/SignUp/client_script.rb
@@ -1,8 +1,44 @@
-require "c:\\dev\\sites\\crunchbase\\shared\.rb"
 require 'rubygems'
 require 'watir'
 
-@browser = browser = Watir::Browser.new
+def solve_captcha
+  image = "#{ENV['USERPROFILE']}\\citation\\crunchbase_captcha.png"
+  obj = @browser.image( :xpath, '/html/body/div[6]/div[2]/div/form/table/tbody/tr[11]/td/div/div/table/tbody/tr[2]/td[2]/div/img' )
+  puts "CAPTCHA source: #{obj.src}"
+  puts "CAPTCHA width: #{obj.width}"
+  obj.save image
+
+  CAPTCHA.solve image, :manual
+end
+
+def enter_captcha
+
+    capSolved = false
+    count = 1
+    until capSolved or count > 5 do
+        captcha_code = solve_captcha
+        @browser.text_field( :id, 'recaptcha_response_field').set captcha_code
+        @browser.button(:name => 'commit').click
+        sleep(2)    
+        if not @browser.text.include? "Error with reCAPTCHA!"
+            capSolved = true
+        end
+    count+=1
+    end
+
+    if capSolved == true
+        true
+    else
+        throw("Captcha was not solved")
+    end
+end
+
+def goto_signup_page
+    puts 'Loading Signup Page for Crunchbase'
+    @browser.goto('http://www.crunchbase.com/account/signup')
+end
+
+@browser = Watir::Browser.new
 #@browser.goto('http://www.crunchbase.com/account/signup')
 goto_signup_page
 
@@ -15,4 +51,5 @@ goto_signup_page
 solve_captcha
 enter_captcha
 @browser.button(:name => 'commit').click
-#browser.link(:href => 'http://www.crunchbase.com/account/confirmation').click
+browser.link(:href => 'http://www.crunchbase.com/account/confirmation').click
+true

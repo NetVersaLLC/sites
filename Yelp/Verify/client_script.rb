ee95be115d6f0141dd0f41e2154a139a534c924e
Kazyyk <Asphexis@gmail.com>
2013-11-22 15:31:37 +0000

    As previous; forgot database_to_git

--- Yelp/Verify/client_script.rb
+++ Yelp/Verify/client_script.rb
@@ -1,3 +1,11 @@
+if :POPAuthenticationError == data['link']
+	self.start("Yelp/Verify",1440)
+	raise StandardError.new "Could not log into email address. Retrying in one day."
+elsif data['link'].nil?
+	self.start("Yelp/Verify",1440)
+	raise StandardError.new "Failed to find confirmation email from Yelp. Retrying in one day."
+end
+
 @browser = Watir::Browser.new :firefox
 
 at_exit do

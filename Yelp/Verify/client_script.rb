3f0b073f49ea58cca492212f2723ae23728ced1b
Kazyyk <Asphexis@gmail.com>
2013-12-27 21:18:10 +0000

    Moved goto

--- Yelp/Verify/client_script.rb
+++ Yelp/Verify/client_script.rb
@@ -1,20 +1,19 @@
 @browser = Watir::Browser.new :firefox
 at_exit do
-	unless @browser.nil?
-		@browser.close
-	end
+    unless @browser.nil?
+        @browser.close
+    end
 end
 
 link = data['link']
 
-@browser.goto(link)
-
 if link.nil?
-	self.start("Yelp/Verify", 1440)
+    self.start("Yelp/Verify", 1440)
     self.success("Could not find e-mail, trying again in 24 hours")
 else
-	Watir::Wait.until? do
- 		@browser.text.include? "Thanks for Submitting your Business to Yelp"
-	end
+    @browser.goto(link)
+    Watir::Wait.until? do
+        @browser.text.include? "Thanks for Submitting your Business to Yelp"
+    end
     self.success("Job completed successfully")
 end

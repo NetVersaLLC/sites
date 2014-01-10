834478d88fb5e85393679a90b39c547ed123674b
Andrew <mr.andrewcarroll@gmail.com>
2014-01-10 09:41:08 -0500

    Update data_generator.rb

--- Listwns/SignUp/data_generator.rb
+++ Listwns/SignUp/data_generator.rb
@@ -1,4 +1,13 @@
 data = {}
 data[ 'email' ]			= business.bings.first.email
-data[ 'password' ]		= Yahoo.make_password
+data[ 'password' ]		= Yahoo.make_password.gsub(/[^a-zA-Z0-9]/,'')
+data['business']		= business.business_name
+data['phone']			= business.local_phone
+data['address']			= business.address
+data['category']		= ListwnsCategory.find(business.listwns.first.category_id).name
+data['city']			= business.city
+data['country']			= "United States"
+data['zip']				= business.zip
+data['description']		= business.business_description
+data['keywords']		= business.category1
 data

c76e724a0e981c36d0fb2fb237d726571708733e
Andrew <mr.andrewcarroll@gmail.com>
2014-01-13 12:45:28 -0500

    Update data_generator.rb

--- Localcensus/AddListing/data_generator.rb
+++ Localcensus/AddListing/data_generator.rb
@@ -1,6 +1,6 @@
 data = {}
-localy = Localcensus.where(:business_id => business.id).first
-data[ 'category' ]          = localy.localcensus_category.name.gsub("\n", "")
+catty                   = Localcensus.where(:business_id=>business.id).first
+data[ 'category' ]      = LocalcensusCategory.find(catty.category_id).name
 data[ 'state' ]			= business.state_name
 data[ 'city' ]			= business.city
 data[ 'business' ]		= business.business_name
@@ -11,6 +11,6 @@ data[ 'phone' ]			= business.local_phone
 data[ 'fax' ]			= business.fax_number
 data[ 'email' ]			= business.bings.first.email
 data[ 'website' ]		= business.company_website
-data[ 'hours' ]			= Getfav.consolidate_hours( business )
+data[ 'hours' ]			= Getfave.consolidate_hours( business )
 data[ 'description' ]		= business.business_description
 data

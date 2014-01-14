45cd6b8b29070e89aaa3f2b13e4259675e487984
Kazyyk <Asphexis@gmail.com>
2014-01-13 20:44:07 -0500

    Some additional condition handling

--- Ziplocal/SignUp/client_script.rb
+++ Ziplocal/SignUp/client_script.rb
@@ -5,22 +5,7 @@ at_exit {
 	end
 }
 
-def add_business( data )
-#adds the business, begins by clicking the "add this number" link
-
-	@browser.link( :text, 'add this number' ).click
-	#fill out the form with client data
-	@browser.text_field( :id, 'listingForm:disp').when_present.set data[ 'business' ]
-	@browser.text_field( :id, 'listingForm:StrAdr').set data[ 'streetnumber' ]
-	@browser.text_field( :id, 'listingForm:city').set data[ 'citystate' ]
-	@browser.text_field( :id, 'listingForm:zip').set data[ 'zip' ]
-	@browser.text_field( :id, 'listingForm:web').set data[ 'website' ]
-	@browser.text_field( :id, 'listingForm:email').set data[ 'business_email' ]
-	@browser.text_field( :id, 'listingForm:_id154').set data[ 'first_name' ] + ' ' + data[ 'last_name' ] 
-	@browser.text_field( :id, 'listingForm:_id159').set data[ 'email' ]
-	@browser.radio( :value, 'O').set
-	@browser.text_field(:id, "listingForm:_id157").set data['prefix'] #Good to add the prefix lately.
-
+def add_category( data )
 	#Now to add categories. At least 1 category is required.
 	@browser.button( :id, 'listingForm:addTest2' ).fire_event("onClick")
 
@@ -33,27 +18,64 @@ def add_business( data )
 
 	@browser.button( :value, 'Add new').when_present.click
 	sleep(3)	
-	if @browser.text_field(:id, 'listingForm:newSic1').text != ""
-		@browser.button( :value, 'Add new').click #Try one more time hth!!
+	until @browser.select_list(:id, 'listingForm:sicList').option(:text, "#{data['categoryKeyword']}").exists?
+		@browser.text_field( :id, 'listingForm:newSic1').focus
+		@browser.text_field( :id, 'listingForm:newSic1').set data[ 'categoryKeyword' ]
+		@browser.button( :value, 'Add new').click # Clicking will continue until morale improves
+		if @retries < 1
+			self.failure("Could not set category.")
+			return false
+		else
+			puts "Category didn't set. Retrying..."
+			@retries -= 1
+		end
 	end
+	return true
+end
+
+def add_business( data )
+#adds the business, begins by clicking the "add this number" link
+	if @retries < 3
+		@browser.refresh
+	else
+		@browser.link( :text, 'add this number' ).when_present.click
+	end
+	#fill out the form with client data
+	@browser.text_field( :id, 'listingForm:disp').when_present.set data[ 'business' ]
+	@browser.text_field( :id, 'listingForm:StrAdr').set data[ 'streetnumber' ]
+	@browser.text_field( :id, 'listingForm:city').set data[ 'citystate' ]
+	@browser.text_field( :id, 'listingForm:zip').set data[ 'zip' ]
+	@browser.text_field( :id, 'listingForm:web').set data[ 'website' ]
+	@browser.text_field( :id, 'listingForm:email').set data[ 'business_email' ]
+	@browser.text_field( :id, 'listingForm:_id154').set data[ 'first_name' ] + ' ' + data[ 'last_name' ] 
+	@browser.text_field( :id, 'listingForm:_id159').set data[ 'email' ]
+	@browser.radio( :value, 'O').set
+	@browser.text_field(:id, "listingForm:_id157").set data['prefix'] #Good to add the prefix lately.
+
+	if add_category(data) == false
+		break
+	end
+
 	sleep(3)
 	@browser.link( :text, 'Save').click
-	sleep 15
-	#Re-check if category was added.
-	if @browser.text.include? "Category is required." #If we get the category is required message we try last time.
-		@browser.button( :value, 'Add new').click
-		sleep 10
-		@browser.link( :text, 'Save').click
-		30.times{ break if @browser.status == "Done"; sleep 1}
-	elsif not @browser.text.include? "Thank you for updating our directory."
-		@browser.link( :text, 'Save').click
-		sleep 10
-	end
-	if @browser.text.include? "Thank you for updating our directory. Updates take effect within 3 business days. Check back for opportunities to upgrade your listing at that time."
-		true
+
+	if @browser.text.include? "We apologize but your order can not be submitted"
+		self.failure("Only one submission permitted per day.")
 	else
-		puts "Payload did not successfully complete"
+		Watir::Wait.until { @browser.text.include? "Thank you for updating our directory." }
+		puts "Business added successfully."
+		self.success
 	end
+rescue => e
+  unless @retries == 0
+    puts "Error caught in add_business: #{e.inspect}"
+    puts "Retrying in two seconds. #{@retries} attempts remaining."
+    sleep 2
+    @retries -= 1
+    retry
+  else
+    self.failure("Error in add_business could not be resolved. Error: #{e.inspect}")
+  end
 end
 
 def main( data )
@@ -69,12 +91,13 @@ def main( data )
 		puts ("Phone number already associated.")
 		if @browser.text.include? "#{data['business']}"
 			self.save_account("Ziplocal",{:listing_url => listing_url.href })
+			self.success("Pre-existing business link grabbed successfully.")
 		else
-			puts "Number associated with different business."
+			self.failure("Phone number associated with a different business.")
 		end
 	else
 		add_business(data)
 	end
-	true
 end
+@retries = 3
 main( data )

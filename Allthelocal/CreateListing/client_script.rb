5d4d383d92a7468eed61cae62f4c632b24812408
Kazyyk <Asphexis@gmail.com>
2014-01-20 04:06:39 -0500

    Methods & Rescue

--- Allthelocal/CreateListing/client_script.rb
+++ Allthelocal/CreateListing/client_script.rb
@@ -1,3 +1,9 @@
+at_exit{
+    unless @browser.nil?
+        @browser.close
+    end
+}
+
 def solve_captcha
   image = "#{ENV['USERPROFILE']}\\citation\\allthelocal_captcha.png"
   @browser.img(:id, "phoca-captcha").save  image
@@ -24,19 +30,34 @@ def enter_captcha
 	end
 end
 
-@browser = Watir::Browser.start  "http://allthelocal.com/add_business.php"
-@browser.text_field(:name, "visitor").set(data['contact_name'])
-@browser.text_field(:name, "visitormail").set(data['email'])
-@browser.text_field(:name, "phone").set(data['local_phone'])
-@browser.text_field(:name, "business").set(data['business_name'])
-@browser.text_field(:name, "address").set(data['address'])
-@browser.text_field(:name, "city").set(data['city'])
-@browser.text_field(:name, "state").set(data['state'])
-@browser.text_field(:name, "zip").set(data['zip'])
-@browser.text_field(:name, "keyword").set(data['keywords'])
-@browser.text_field(:name, "url").set(data['company_website'])
-@browser.text_field(:name, "notes").set(data['business_description'])
+def do_the_time_warp_again(data)
+	@browser = Watir::Browser.start  "http://allthelocal.com/add_business.php"
+	@browser.text_field(:name, "visitor").set(data['contact_name'])
+	@browser.text_field(:name, "visitormail").set(data['email'])
+	@browser.text_field(:name, "phone").set(data['local_phone'])
+	@browser.text_field(:name, "business").set(data['business_name'])
+	@browser.text_field(:name, "address").set(data['address'])
+	@browser.text_field(:name, "city").set(data['city'])
+	@browser.text_field(:name, "state").set(data['state'])
+	@browser.text_field(:name, "zip").set(data['zip'])
+	@browser.text_field(:name, "keyword").set(data['keywords'])
+	@browser.text_field(:name, "url").set(data['company_website'])
+	@browser.text_field(:name, "notes").set(data['business_description'])
+rescue => e
+  unless @retries == 0
+    puts "Error caught in do_the_time_warp_again: #{e.inspect}"
+    puts "Retrying in two seconds. #{@retries} attempts remaining."
+    sleep 2
+    @retries -= 1
+    retry
+  else
+    raise "Error in do_the_time_warp_again could not be resolved. Error: #{e.inspect}"
+  end
+end
 
+# Main Controller
+@retries = 3
+do_the_time_warp_again(data)
 enter_captcha()
 
 puts "Listing Submission Done!"

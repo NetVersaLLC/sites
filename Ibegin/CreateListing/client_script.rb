82fcbdadfabe1a20219d509bfe58acd3d1b61798
Kazyyk <Asphexis@gmail.com>
2014-01-21 14:04:06 -0500

    Cleaned up

--- Ibegin/CreateListing/client_script.rb
+++ Ibegin/CreateListing/client_script.rb
@@ -1,38 +1,37 @@
+# Developer Notes
+# nil
+
+# Browser code
 @browser = Watir::Browser.new :firefox
 at_exit{
   unless @browser.nil?
-    @broser.close
+    @browser.close
   end
 }
 
+# Methods
 def sign_in( data )
+  @browser.goto( 'http://www.ibegin.com/account/login/' )
+  @browser.text_field( :name, 'name' ).set data['email']
+  @browser.text_field( :name, 'pw' ).set data['password']
 
-retries = 5
+  @browser.button( :value, /Login/i).click
 
-  begin
-    @browser.goto( 'http://www.ibegin.com/account/login/' )
-    @browser.text_field( :name, 'name' ).set data['email']
-    @browser.text_field( :name, 'pw' ).set data['password']
+  Watir::Wait.until { @browser.link(:text => 'Logout').exists? }
+end
 
-    @browser.button( :value, /Login/i).click
-
-    sleep 2
-    Watir::Wait.until { @browser.link(:text => 'Logout').exists? }
-    return true
-  rescue
-    if retries > 0
-          puts "There was an error with the SignUp. Retrying in 5 seconds."
-          retries -= 1
-          sleep 5
-          retry
-      else
-          puts "After 5 retries the payload could not sign in. Data available:"
-          puts data
-          puts "Data required:"
-          puts "email,password"
-          throw("Job failed during login. Verify the credintials are valid")
-      end
-  end
+def initial_signup(data)
+  @browser.goto('http://www.ibegin.com/business-center/submit/')
+  @browser.text_field( :name, 'name').when_present.set data['business_name']
+  @browser.select_list( :id, 'country').select data['country']
+  sleep(3)
+  @browser.select_list( :id, 'region').select data['state_name']
+  sleep(3)
+  @browser.select_list( :id, 'city').option(:text => /#{data['city']}/i).click
+  @browser.text_field( :name, 'address').set data['address']
+  @browser.text_field( :name, 'zip').set data['zip']
+  @browser.text_field( :name, 'phone').set data['phone']
+  @browser.text_field( :name, 'fax').set data['fax']
 end
 
 def loop_cats(data)
@@ -57,70 +56,59 @@ def loop_cats(data)
 
 end
 
-sign_in( data )
+def set_category(data)
+  puts "Category: #{data['category1']}"
+  category = data[ 'category1' ]
+  query = data[ 'category1' ]
+  wordcount = data[ 'category1' ].split(" ").size
+  count = 0
+  #Categories
+  #Category selection involves a popup window that must be attached to than the category searched for, then selected.
+  # If the category cannot be found it will split the category into single words and search word by word until it finds the category
+  @browser.div( :id => 'id_category1_wrap', :index => 0 ).link( :title, 'Select' ).click
 
-@browser.goto('http://www.ibegin.com/business-center/submit/')
-sleep(3)
-@browser.text_field( :name, 'name').set data['business_name']
-@browser.select_list( :id, 'country').select data['country']
-sleep(3)
-@browser.select_list( :id, 'region').select data['state_name']
-sleep(3)
-@browser.select_list( :id, 'city').option(:text => /#{data['city']}/i).click
-@browser.text_field( :name, 'address').set data['address']
-@browser.text_field( :name, 'zip').set data['zip']
-@browser.text_field( :name, 'phone').set data['phone']
-@browser.text_field( :name, 'fax').set data['fax']
-category = data[ 'category1' ]
-query = data[ 'category1' ]
-wordcount = data[ 'category1' ].split(" ").size
-count = 0
-#Categories
-#Category selection involves a popup window that must be attached to than the category searched for, then selected.
-# If the category cannot be found it will split the category into single words and search word by word until it finds the category
-@browser.div( :id => 'id_category1_wrap', :index => 0 ).link( :title, 'Select' ).click
+  sleep 2
+  Watir::Wait.until { @browser.window( :title, "Categories Selector | iBegin").exists? }
 
-sleep 2
-Watir::Wait.until { @browser.window( :title, "Categories Selector | iBegin").exists? }
-
-@browser.window( :title, "Categories Selector | iBegin").when_present.use do
-	@browser.text_field( :id, 'id_q').set query
-	@browser.button( :value, 'Go').click
-	sleep 4
-	if @browser.link(:text => "#{data['category1']}").exists?
-		@browser.link(:text => "#{data['category1']}").click
-	else
-		loop_cats(data)
-	end		
+  @browser.window( :title, "Categories Selector | iBegin").when_present.use do
+    @browser.text_field( :id, 'id_q').set query
+    @browser.button( :value, 'Go').click
+    sleep 4
+    if @browser.link(:text => "#{data['category1']}").exists?
+      @browser.link(:text => "#{data['category1']}").click
+    else
+      loop_cats(data)
+    end   
+  end
 end
 
+def set_socialmedia(data)
+  @browser.text_field( :name, 'url').set data['url']
+  @browser.text_field( :name, 'facebook').set data['facebook']
+  @browser.text_field( :name, 'twitter_name').set data['twitter_name']
+  @browser.text_field( :name, 'desc').set data['desc']
+  @browser.text_field( :name, 'brands').set data['brands']
+  @browser.text_field( :name, 'products').set data['products']
+  @browser.text_field( :name, 'services').set data['services']
+end
 
-
-sleep 5
-
-data[ 'payment_methods' ].each{ | method |
+def set_pay_methods(data)
+  data[ 'payment_methods' ].each{ | method |
     @browser.checkbox( :id => /#{method}/ ).click
   }
-
- @browser.text_field( :name, 'url').set data['url']
-@browser.text_field( :name, 'facebook').set data['facebook']
-@browser.text_field( :name, 'twitter_name').set data['twitter_name']
-@browser.text_field( :name, 'desc').set data['desc']
-@browser.text_field( :name, 'brands').set data['brands']
-@browser.text_field( :name, 'products').set data['products']
-@browser.text_field( :name, 'services').set data['services']
-
-@browser.button( :value => /Submit Business/).click
-
-sleep 2
-if Watir::Wait.until { @browser.text.include? "Congratulations" } ==true
-  puts "Initial registration is sucessfull"
-else
-  raise Error "Initial registration is unsucessfull"
 end
+
+# Main Controller
+sign_in(data)
+initial_signup(data)
+set_category(data)
+set_socialmedia(data)
+set_pay_methods(data)
+@browser.button( :value => /Submit Business/).click
+Watir::Wait.until { @browser.text.include? "Congratulations" }
 
 if @chained
-	self.start("Ibegin/Notify")
+  self.start("Ibegin/Notify")
 end
 
-true
+self.success

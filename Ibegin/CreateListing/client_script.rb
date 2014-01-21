4f6323600c8406ed2fed3ba62fedb445fd930450
Kazyyk <Asphexis@gmail.com>
2014-01-21 12:17:03 -0500

    Added browser code

--- Ibegin/CreateListing/client_script.rb
+++ Ibegin/CreateListing/client_script.rb
@@ -1,4 +1,61 @@
+@browser = Watir::Browser.new :firefox
+at_exit{
+  unless @browser.nil?
+    @broser.close
+  end
+}
 
+def sign_in( data )
+
+retries = 5
+
+  begin
+    @browser.goto( 'http://www.ibegin.com/account/login/' )
+    @browser.text_field( :name, 'name' ).set data['email']
+    @browser.text_field( :name, 'pw' ).set data['password']
+
+    @browser.button( :value, /Login/i).click
+
+    sleep 2
+    Watir::Wait.until { @browser.link(:text => 'Logout').exists? }
+    return true
+  rescue
+    if retries > 0
+          puts "There was an error with the SignUp. Retrying in 5 seconds."
+          retries -= 1
+          sleep 5
+          retry
+      else
+          puts "After 5 retries the payload could not sign in. Data available:"
+          puts data
+          puts "Data required:"
+          puts "email,password"
+          throw("Job failed during login. Verify the credintials are valid")
+      end
+  end
+end
+
+def loop_cats(data)
+  catarray = data['category1'].split(" ")
+  wordcount = catarray.length
+  count = 0
+  cat_found = false
+
+  while wordcount > count && cat_found != true
+    @browser.window( :title, "Categories Selector | iBegin").when_present.use do
+      query = catarray[count]
+      @browser.text_field( :id, 'id_q').set query
+      @browser.button( :value, 'Go').click
+      sleep 5
+      if @browser.link(:text => /#{query}/i).exists?
+        @browser.link(:text => /#{query}/i).click
+        cat_found =true
+      end
+      count+=1
+    end
+  end
+
+end
 
 sign_in( data )
 

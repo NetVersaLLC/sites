515e1e397891b54856f0e6603bbc065d7ffc6cce
Kazyyk <Asphexis@gmail.com>
2014-01-21 12:18:12 -0500

    Browser code

--- Ibegin/Verify/client_script.rb
+++ Ibegin/Verify/client_script.rb
@@ -1,3 +1,40 @@
+@browser = Watir::Browser.new :firefox
+at_exit{
+  unless @browser.nil?
+    @browser.close
+  end
+}
+
+def sign_in( data )
+
+retries = 5
+
+  begin
+    @browser.goto( 'http://www.ibegin.com/account/login/' )
+    @browser.text_field( :name, 'name' ).set data['email']
+    @browser.text_field( :name, 'pw' ).set data['password']
+
+    @browser.button( :value, /Login/i).click
+
+    sleep 2
+    Watir::Wait.until { @browser.link(:text => 'Logout').exists? }
+    return true
+  rescue
+    if retries > 0
+          puts "There was an error with the SignUp. Retrying in 5 seconds."
+          retries -= 1
+          sleep 5
+          retry
+      else
+          puts "After 5 retries the payload could not sign in. Data available:"
+          puts data
+          puts "Data required:"
+          puts "email,password"
+          throw("Job failed during login. Verify the credintials are valid")
+      end
+  end
+end
+
 sign_in( data )
 sleep(4)
 

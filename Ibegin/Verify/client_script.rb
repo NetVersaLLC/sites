d20423ee278cf02041891703c256ec19c18e0727
Kazyyk <Asphexis@gmail.com>
2014-01-21 15:20:16 -0500

    Cleaned up

--- Ibegin/Verify/client_script.rb
+++ Ibegin/Verify/client_script.rb
@@ -1,3 +1,8 @@
+# Developer Notes
+# nil
+
+
+# Browser code
 @browser = Watir::Browser.new :firefox
 at_exit{
   unless @browser.nil?
@@ -5,87 +10,55 @@ at_exit{
   end
 }
 
+# Methods
 def sign_in( data )
+  @browser.goto( 'http://www.ibegin.com/account/login/' )
+  @browser.text_field( :name, 'name' ).set data['email']
+  @browser.text_field( :name, 'pw' ).set data['password']
+  @browser.button( :value, /Login/i).click
 
-retries = 5
-
-  begin
-    @browser.goto( 'http://www.ibegin.com/account/login/' )
-    @browser.text_field( :name, 'name' ).set data['email']
-    @browser.text_field( :name, 'pw' ).set data['password']
-
-    @browser.button( :value, /Login/i).click
-
+  Watir::Wait.until { @browser.link(:text => 'Logout').exists? }
+rescue => e
+  unless @retries == 0
+    puts "Error caught in sign_in: #{e.inspect}"
+    puts "Retrying in two seconds. #{@retries} attempts remaining."
     sleep 2
-    Watir::Wait.until { @browser.link(:text => 'Logout').exists? }
-    return true
-  rescue
-    if retries > 0
-          puts "There was an error with the SignUp. Retrying in 5 seconds."
-          retries -= 1
-          sleep 5
-          retry
-      else
-          puts "After 5 retries the payload could not sign in. Data available:"
-          puts data
-          puts "Data required:"
-          puts "email,password"
-          throw("Job failed during login. Verify the credintials are valid")
-      end
+    @retries -= 1
+    retry
+  else
+    raise "Error in sign_in could not be resolved. Error: #{e.inspect}"
   end
 end
 
-sign_in( data )
-sleep(4)
+def do_the_thing(data)
 
 @browser.goto("http://www.ibegin.com/business-center/")
-
 @browser.link(:text => 'Claim Now').click
 Watir::Wait.until { @browser.button(:value => 'Click to Call Your Phone & Receive Code').exists? }
 retries = 5
-begin
-	@browser.button( :value => /Click to Call Your Phone & Receive Code/i).click
-rescue
-	if retries > 0
-       puts "Button was not clicked/found. Retrying in 2 seconds."
-       retries -= 1
-       sleep 2
-       retry
-   else
-       puts "After 5 retries the button could not be clicked."
-   end
+
+@browser.button( :value => /Click to Call Your Phone & Receive Code/i).click
+code = PhoneVerify.retrieve_code('Ibegin')
+
+@browser.text_field( :name => 'verification_code').set code
+@browser.button( :value => /Submit/).click
+
+Watir::Wait.until { @browser.text.include? "Congratulations! Your phone number has been verified."}	
+
+rescue => e
+  unless @retries == 0
+    puts "Error caught in do_the_thing: #{e.inspect}"
+    puts "Retrying in two seconds. #{@retries} attempts remaining."
+    sleep 2
+    @retries -= 1
+    retry
+  else
+    raise "Error in do_the_thing could not be resolved. Error: #{e.inspect}"
+  end
 end
 
-
-begin
-	@browser.button( :value => /Click to Call Your Phone & Receive Code/i).click
-	code = PhoneVerify.retrieve_code('Ibegin')
-
-	@browser.text_field( :name => 'verification_code').set code
-	@browser.button( :value => /Submit/).click
-
-	sleep 2
-	Watir::Wait.until(10) { @browser.text.include? "Congratulations! Your phone number has been verified."}	
-true
-rescue
-	if retries > 0
-		if @browser.text.include? "The code you entered did not match. Please re-try below"
-       		puts "The code is incorrect."
-		PhoneVerify.wrong_code('Ibegin')
-       	end
-       	puts("Phone Verify Failed, retrying in 2 seconds.")
-       retries -= 1
-       sleep 2
-       retry
-   else
-       puts "After 5 retries the button could not be clicked."
-   end
-end
-
-
-
-
-
-
-
-
+# Main Controller
+sign_in( data )
+do_the_thing( data )
+self.save_account
+self.success

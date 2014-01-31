cb84000d3ef1838d6121a037b835c44b998dea05
Jahangir Zinedine <jzinedine@gmail.com>
2014-01-31 22:47:20 +0330

    Foursquare issues fixed
    map selection fixed
    twitter username fetched correctly
    birthdate split with - not /

--- Foursquare/AddListing/client_script.rb
+++ Foursquare/AddListing/client_script.rb
@@ -5,11 +5,14 @@ class Runner
     @data= data
     @brow = Watir::Browser.new :firefox
     try_do :login, 2
+    sleep 5
     try_do :claim, 3
+    sleep 5
     try_do :check_results, 2
-    if @brow.url.start_with?("https://foursquare.com/venue/claim?venuename")
-      try_do :add_venue, 2
-      try_do :claim_venue, 2
+    sleep 5
+    if @brow.url.start_with?("https://foursquare.com/add_venue")
+      try_do :add_venue, 3
+      try_do :claim_venue, 4
     else
       # @self.start('Foursquare/ClaimListing')
     end
@@ -17,72 +20,19 @@ class Runner
     @brow.close if @brow
   end
   
+  # First Step
   def login
     @brow.goto 'https://foursquare.com/login'
+    return true if @brow.url == 'https://foursquare.com/'
     txt_set({:id=> 'username'}, @data['email'])
     txt_set({:id=> 'password'}, @data['password'])
     @brow.button(:id=> 'loginFormButton').click
-    Watir::Wait.while{ @brow.button(:id=> 'loginFormButton').exists? }
+    Watir::Wait.while{ @brow.button(:id=> 'loginFormButton').present? }
     wait_for_page_load
     true
   end
   
-  def add_venue
-    wait_for_page_load
-    txt_set({:id => 'venueAddress_field'}, @data['address'])
-    txt_set({:id => 'venueZip_field'}, @data['zip'])
-    dispatch_event({:id=> 'venueAddress_field'}, 'blur')
-    txt_set({:id => 'venueTwitterName_field'}, @data['twitter_page'])
-    txt_set({:id => 'venuePhone_field'}, @data['phone'])
-    select_text({:name => 'topLevelCategory'}, @data['category1'])
-    @brow.select_list(:name => 'topLevelCategory').click
-    Watir::Wait.until{ @brow.select_list(:name => 'secondLevelCategory').present? }
-    select_text({:name => 'secondLevelCategory'}, @data['category2'])
-    # select_text({:name => 'thirdLevelCategory'}, @data['category3'])
-    try_do :set_map, 2
-    @brow.button(:value=> 'Save').click
-    Watir::Wait.while{ @brow.button(:value=> 'Save').present? }
-    true
-  end
-  
-  def set_map
-    if @brow.hidden(:name=>'geolng').value + @brow.hidden(:name=>'geolat').value == ''
-      zoom= @brow.execute_script('return map._zoom;')
-      if zoom < 16
-        @brow.execute_script("setTimeout(function(){
-          if(map._zoom < 16)
-            map.zoomIn();
-          }, 1000)")
-        #@brow.link(:title => "Zoom in").click
-      end
-      sleep 5
-      @brow.images(:class => 'leaflet-tile leaflet-tile-loaded').first.click          
-      sleep 5
-    end
-    @brow.hidden(:name=>'geolng').value + @brow.hidden(:name=>'geolat').value != ''
-  end
-  
-  def claim_venue   
-    @brow.checkbox(:id => 'agree').set
-    @brow.span(:class => 'continueButton greenButton biggerButton').click
-    Watir::Wait.until{ @brow.element(:id=> 'phoneField').exists? }
-    txt_set({:id => 'phoneField'}, @data['mobile'])
-    @brow.span(:class => 'continueButton greenButton biggerButton').click
-    Watir::Wait.until{ @brow.text_field(:id=> 'phoneField').value.length == 4 }
-    PhoneVerify.send_code("Foursquare", @brow.text_field(:id=> 'phoneField').value)
-    Watir::Wait.until{ @brow.element(:id=> 'option2').exists? }
-    @brow.radio(:id=> 'option2').set
-    @brow.a(:text=> /Mail me/).click
-  end
-  
-  def check_results
-    wait_for_page_load
-    @brow.execute_script('return document.getElementsByClassName("addVenueLink")[0].click();')
-    Watir::Wait.while{ @brow.a(:class=> 'addVenueLink').exists? }
-    wait_for_page_load
-    true
-  end
-  
+  # Second Step
   def claim
     claim_url= 'https://foursquare.com/venue/claim'
     @brow.goto claim_url
@@ -95,6 +45,72 @@ class Runner
     sleep 5
     Watir::Wait.until{ @brow.a(:class=> 'addVenueLink').exists? }
     true
+  end
+  
+  # Third Step
+  def check_results
+    wait_for_page_load
+    @brow.execute_script('return document.getElementsByClassName("addVenueLink")[0].click();')
+    Watir::Wait.while{ @brow.a(:class=> 'addVenueLink').present? }
+    sleep 5
+    wait_for_page_load
+    true
+  end
+  
+  # Fourth Step
+  def add_venue
+    wait_for_page_load
+    txt_set({:id => 'venueAddress_field'}, @data['address'])
+    txt_set({:id => 'venueZip_field'}, @data['zip'])
+    dispatch_event({:id=> 'venueAddress_field'}, 'blur')
+    txt_set({:id => 'venueTwitterName_field'}, @data['twitter_page'])
+    txt_set({:id => 'venuePhone_field'}, @data['phone'])
+    puts @data['category1']
+    puts @data['category2']
+    select_text({:name => 'topLevelCategory'}, @data['category2'])
+    #@brow.select_list(:name => 'topLevelCategory').click
+    Watir::Wait.until{ @brow.select_list(:name => 'secondLevelCategory').exists? }
+    select_text({:name => 'secondLevelCategory'}, @data['category1'])
+    # select_text({:name => 'thirdLevelCategory'}, @data['category3'])
+    try_do :set_map, 3
+    @brow.button(:value=> 'Save').click
+    Watir::Wait.while{ @brow.button(:value=> 'Save').present? }
+    true
+  end
+  
+  def set_map
+    zoom= @brow.execute_script('return map._zoom;')
+    if zoom < 16
+      @brow.execute_script("setTimeout(function(){
+        if(map._zoom < 16)
+          map.zoomIn();
+        }, 1000)")
+      sleep 5
+    end
+    puts @brow.images(:class=> 'leaflet-tile-loaded').length
+    @brow.images(:class => 'leaflet-tile-loaded').first.click   
+    @brow.hidden(:name=>'geolng').value + @brow.hidden(:name=>'geolat').value != ''
+  end
+  
+  # Fifth Step
+  def claim_venue   
+    @brow.checkbox(:id => 'agree').set
+    @brow.span(:class => 'continueButton greenButton biggerButton').click
+    Watir::Wait.until{ @brow.text_field(:id=> 'phoneField').exists? }
+    txt_set({:id => 'phoneField'}, @data['mobile'])
+    @brow.span(:class => 'continueButton greenButton biggerButton').click
+    Watir::Wait.until{ @brow.text_field(:id=> 'phoneField').value.length == 4 }
+    try_do :phone_verify, 3, @brow.text_field(:id=> 'phoneField').value
+    Watir::Wait.until{ @brow.element(:id=> 'option2').exists? }
+    @brow.radio(:id=> 'option2').set
+    @brow.a(:text=> /Mail me/).click
+  end
+
+  def phone_verify code
+     puts code   
+     PhoneVerify.send_code("Foursquare", code) 
+     sleep 30
+     true
   end
   
   def txt_set(selector, keys)
@@ -151,9 +167,4 @@ end
 runner= Runner.new
 runner.main(data)
 
-sleep 2
-if @chained
-  self.start("Foursquare/Verify")
-  self.start("Foursquare/AddListing")
-end
 true

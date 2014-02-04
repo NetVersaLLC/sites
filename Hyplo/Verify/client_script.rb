8e05882167306c72980dfa62685bd08f00993a87
Kazyyk <Asphexis@gmail.com>
2014-02-04 16:03:28 -0500

    Rewrite to retry

--- Hyplo/Verify/client_script.rb
+++ Hyplo/Verify/client_script.rb
@@ -1,5 +1,11 @@
-@browser.goto(data['url'])
-	if @chained
-		self.start("Hyplo/AddListing")
-	end
-true
+unless data['url'].nil?
+  @browser.goto(data['url'])
+	  if @chained
+		  self.start("Hyplo/AddListing")
+	  end
+else
+  if @chained
+    self.start("Hyplo/Verify", 1440)
+  end
+end
+self.success

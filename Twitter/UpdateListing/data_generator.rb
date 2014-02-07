67c91ca195ebd434029affa27962219640c55f11
Kazyyk <Asphexis@gmail.com>
2014-02-07 04:38:40 -0500

    Added required data

--- Twitter/UpdateListing/data_generator.rb
+++ Twitter/UpdateListing/data_generator.rb
@@ -2,5 +2,9 @@ data = {}
 data['username']	= business.twitters.first.username
 data['password']	= business.twitters.first.password
 data['tweet'] 		= business.status_message
+data['website']   = business.company_website
+data['bio']       = business.business_description
+data['name']      = business.name
+data['state']     = business.state_name
 data
 

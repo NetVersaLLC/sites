904b804d7637c4fdc50a0d064ff13c80b5a747be
Kazyyk <Asphexis@gmail.com>
2014-02-07 15:49:27 -0500

    Added necessary data

--- Ziplocal/UpdateListing/data_generator.rb
+++ Ziplocal/UpdateListing/data_generator.rb
@@ -5,7 +5,16 @@ data[ 'last_name' ] = business.contact_last_name
 data[ 'email' ] = business.bings.first.email
 data[ 'business_email' ] = business.bings.first.email
 data[ 'business' ] = business.business_name
+data[ 'streetnumber' ] = business.address + ' ' + business.address2
+data[ 'city' ] = business.city
+data[ 'state' ] = business.state_name
+data[ 'stateabreviation' ] = business.state
+data[ 'zip' ] = business.zip
+data[ 'country' ] = 'United States'
+data[ 'countryAbrv' ] = 'US'
+data[ 'citystate' ] = data[ 'city' ] + ', ' + data[ 'stateabreviation' ]
 data[ 'website' ] = business.company_website
-catty 			= Ziplocal.where(:business_id => business.id).first
-data[ 'categoryKeyword' ] = ZiplocalCategory.find(catty.category_id).name
+#catty = Ziplocal.where(:business_id => business.id).first
+#data[ 'categoryKeyword' ] = ZiplocalCategory.find(catty.category_id).name
+data[ 'prefix' ] = business.contact_prefix
 data

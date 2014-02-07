4268c01c76375d2ac80d4929782b7f79c46ea7c2
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-07 10:17:52 -0500

    Added services offered.

--- Digabusiness/AddListing/data_generator.rb
+++ Digabusiness/AddListing/data_generator.rb
@@ -1,16 +1,12 @@
 data = {}
-catty                       = Digabusiness.where(:business_id => business.id).first
-catty 						= DigabusinessCategory.find(catty.category_id)
-data[ 'category1' ]         = catty.parent.name.chomp
-data[ 'category2' ]         = catty.name.chomp
-
 data['password']		= business.digabusinesses.first.password
-data[ 'email' ]			= business.digabusinesses.first.email
+data['email' ]			= business.digabusinesses.first.email
 data['username']		= business.digabusinesses.first.username
 
 data[ 'business' ]		= business.business_name
 data[ 'website' ]		= business.company_website
 data[ 'description' ]	= business.business_description
+data[ 'category_id' ]           = Digabusiness.where(:business_id => business.id).first.id
 data[ 'fname' ]			= business.contact_first_name
 data[ 'lname' ]			= business.contact_last_name
 data[ 'fullname' ]		= data[ 'fname' ] + ' ' + data[ 'lname' ]
@@ -20,4 +16,5 @@ data[ 'state_name' ]	= business.state_name
 data[ 'zip' ]			= business.zip
 data[ 'phone' ]			= business.local_phone
 data[ 'payments' ]		= Digabusiness.payment_methods(business)
+data[ 'services' ]              = business.services_offered
 data

570f3e66c3756921a3e93f401cce6d0280f31fa5
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-08 14:51:23 -0500

    remove newlines from cat names

--- Ibegin/CreateListing/data_generator.rb
+++ Ibegin/CreateListing/data_generator.rb
@@ -10,7 +10,7 @@ data[ 'address' ]		= business.address + ' ' + business.address2
 data[ 'zip' ]			= business.zip
 data[ 'phone' ]			= business.local_phone
 data[ 'fax' ]			= business.fax_number
-data[ 'category1' ] 	= IbeginCategory.find(catty.category_id).name
+data[ 'category' ] 	= IbeginCategory.find(catty.category_id).name.chomp
 data[ 'url' ]			= business.company_website
 data[ 'facebook' ]		= ""#business.facebooks.first.facebook_url
 data[ 'twitter_name' ]	= ""#business.twitters.first.twitter_url
@@ -20,3 +20,4 @@ data[ 'products' ]		= ""#business.get_products
 data[ 'services' ]		= business.services_offered
 data[ 'payment_methods' ]	= Ibegin.payment_methods( business )
 data
+

9510283f0f42489ebbcb814d8513832ed89acf87
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-11 11:48:54 -0500

    new category model and image uploads.

--- Magicyellow/UpdateListing/data_generator.rb
+++ Magicyellow/UpdateListing/data_generator.rb
@@ -1,9 +1,9 @@
 data = {}
-data['email']		= business.magicyellows.first.email
-data['password']	= business.magicyellows.first.password
-catty                       = Magicyellow.where(:business_id => business.id).first
-data[ 'business_category' ]       = catty.magicyellow_category.name
+#seed = Random.new.rand(1000..2000).to_s
+data[ 'business_category' ] = MagicyellowCategory.find(business.magicyellows.first.category_id).name
 data[ 'phone' ] = business.local_phone
+data[ 'email' ] = business.bings.first.email
+data[ 'password' ] = business.magicyellows.first.password
 data[ 'business' ] = business.business_name
 data[ 'address' ] = business.address
 data[ 'city' ] = business.city
@@ -13,11 +13,12 @@ data[ 'first_name' ] = business.contact_first_name
 data[ 'last_name' ] = business.contact_last_name
 data[ 'claim_business'] = 'yes'
 data[ 'fullname' ] = data[ 'first_name' ] + " " + data[ 'last_name' ]
-data[ 'business_description']  = (business.business_description + business.category1 + " " + business.category2 + " " + business.category3 + " " + business.category4 + " " + business.category5)[0..200]
-data[ 'payment_option'] = Yahoo.payment_methods(business)
+data[ 'business_description']  = (business.business_description + business.category_description)[0..200]
+data[ 'payment_option'] = Magicyellow.payments(business)
 data[ 'relation_to_business' ] = 'Agent'
 data[ 'is_owner'] = 'Yes'
-data[ 'additional_services' ] = ' '
+data[ 'additional_services' ] = business.services_offered
 data[ 'additional_products' ] = ' '
-data[ 'additional_brands' ] = ' '
+data[ 'additional_brands' ] = business.brands
 data
+

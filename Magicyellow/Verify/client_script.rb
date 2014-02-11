627a38066cc5430d0560f10cb52239b985558816
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-11 13:44:06 -0500

    get password from bing email directly (no pop)

--- Magicyellow/Verify/client_script.rb
+++ Magicyellow/Verify/client_script.rb
@@ -1,10 +1,37 @@
-sign_in(data)
+@browser = Watir::Browser.new :firefox
+at_exit do
+  unless @browser.nil?
+    @browser.close
+  end
+end
 
-sleep(2)
-Watir::Wait.until { @browser.text.include? "Your Contact Info" }
+def get_password_from_email(data)
+  @browser.goto("https://mail.live.com/")
+  @browser.text_field(:name => "login").set data['email']
+  @browser.text_field(:name => 'passwd').set data['bing_password']
+  @browser.form(:name => "f1").button.click
 
-RestClient.post "#{@host}/accounts.json?auth_token=#{@key}&business_id=#{@bid}", 'account[username]' => data['email'], 'account[password]' => data['password'], 'model' => 'Magicyellow'
+  puts 'polling afaor verification email'
+  quit_time = Time.now + 5 * 60;
+  until @browser.ul(:class => "InboxTableBody").link(:text => /MagicYellow.com Business Center Login/).exist?
+    return false if Time.now > quit_time
+    folder = (folder == /Junk/ ? /Inbox/ : /Junk/)
+    sleep 5
+    puts 'switching folder and checking'
+    @browser.ul(:id => "folderListControlUl").link(:title => folder).click
+  end
 
-true
+  puts 'email found'
+  @browser.ul(:class => "InboxTableBody").link(:text => /MagicYellow.com Business Center Login/).click
 
+  @browser.div(:text => /login and password/).text.match(/Password: (\S{6})/)[1]
+end 
+
+password = get_password_from_email(data)
+
+self.save_account("Magicyellow", {:email => data['email'], :password => password })
+if @chained
+  self.start("Magicyellow/UpdateListing")
+end
+self.success
 

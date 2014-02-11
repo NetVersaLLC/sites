b9ab5932117e8c394e2449357d27679863ce292b
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-11 11:47:52 -0500

    add image uploads and return lising url

--- Magicyellow/UpdateListing/client_script.rb
+++ Magicyellow/UpdateListing/client_script.rb
@@ -1,20 +1,56 @@
-sign_in(data)
-
-sleep(3)
-Watir::Wait.until { @browser.text.include? "Your Contact Info" }
-
-@browser.link(:xpath => "/html/body/table/tbody/tr/td/table/tbody/tr/td/table[3]/tbody/tr/td[3]/table/tbody/tr[2]/td[2]/a").click
-
- @browser.text_field(:name => 'ServicesExtra').set data[ 'additional_services' ] 
-  @browser.text_field(:name => 'ProductsExtra').set data[ 'additional_products' ] 
-  @browser.text_field(:name => 'BrandsExtra').set data[ 'additional_brands' ] 
-  
-  # Add payment_options
-  payments = data['payment_option']
-  payments.each do |payment|  
-    @browser.checkbox(:value => card_type(payment).to_s).click
+@browser = Watir::Browser.new :firefox
+at_exit do
+  unless @browser.nil?
+    @browser.close
   end
+end
+def sign_in(data)
+  @browser.goto("http://www.magicyellow.com/login.cfm")
+  @browser.text_field(:id => 'login').set data['email']
+  @browser.text_field(:name => 'password').set data['password']
+  @browser.button(:value => 'Submit').click
+end
 
-@browser.button(:value => 'Save').click
+def remove_images 
+  delete = @browser.link(:text => "Delete")
+  if delete.exist?
+    delete.click
+    remove_images
+  end 
+end 
 
-true
+def update_listing(data)
+  @browser.link(:href => /bizcentereditprofile/).click
+
+  @browser.text_field(:name => 'ServicesExtra').set data[ 'additional_services' ]
+  @browser.text_field(:name => 'ProductsExtra').set data[ 'additional_products' ]
+  @browser.text_field(:name => 'BrandsExtra').set data[ 'additional_brands' ]
+
+  @browser.checkboxes(:name => 'biz_payment_option_type').each{|cb| cb.clear}
+  data['payment_option'].each do |p|
+    @browser.checkbox(:name => 'biz_payment_option_type', :value => p).set
+  end 
+  @browser.button(:value => 'Save').click
+
+  @browser.link(:href => /bizcenterphotos/).click
+  bizid = @browser.url.match(/bizid=(\d+)\z/)[1]
+  remove_images
+
+  @browser.goto "http://www.magicyellow.com/bizcenterphotos_manual.cfm?bizid=#{bizid}"
+  self.images.each do |p|
+    f = File.join("#{ENV['USERPROFILE']}\\citation\\#{@bid}\\images", p)
+    puts "sending image #{f}"
+    @browser.file_field(:name => 'img1').set f 
+    @browser.button(:value => "Upload selected files!").click
+  end 
+  @browser.goto "http://www.magicyellow.com/bizcenter.cfm"
+
+  "http://www.magicyellow.com" + @browser.link(:href => /\/profile/).href
+end 
+
+sign_in(data)
+listing_url = update_listing(data)
+
+self.save_account("Magicyellow", {:listing_url => listing_url, :status => "Listing updated."})
+self.success
+

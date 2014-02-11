ceb191704a29b6977915ff5b696e54e3058d13a9
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-11 13:40:01 -0500

    bug fixes

--- Magicyellow/CreateListing/client_script.rb
+++ Magicyellow/CreateListing/client_script.rb
@@ -1,14 +1,51 @@
-def add_new_business(data)
-  # First Step
-  @browser.select_list(:name => 'thirdParty').select(/#{data[ 'is_owner' ]}/)
-  if data[ 'is_owner'] == 'No'
-    @browser.text_field(:name => 'regNameRelation').set data[ 'relation_to_business' ]
+@browser = Watir::Browser.new :firefox
+at_exit {
+  unless @browser.nil?
+    @browser.close
   end
+}
+
+def search_by_phone(data)
+  @browser.text_field(:name => 'phone').set data[ 'phone' ]
+  @browser.button(:value=> 'Search').click
+  Watir::Wait.until do 
+    @browser.html.include?("<i>No matches found for <b>") || @browser.link(:href => /add-your-business/).exist?
+  end
+  @browser.link(:href => /add-your-business/).exist?
+end
+
+#Claim business
+def claim_business(data)
+  if @browser.link(:text=> 'Claim This Business').exist?
+    @browser.link(:text=> 'Claim This Business').click
+    add_new_business(data)
+  else
+    puts "Business is already claimed"
+  end
+end
+
+def sign_in(data)
+  @browser.goto("http://www.magicyellow.com/login.cfm")
+  @browser.text_field(:id => 'login').set data['email']
+  @browser.text_field(:name => 'password').set data['password']
+  @browser.button(:value => 'Submit').click
+end
+
+def captcha_text(image_element) 
+  puts "captcha solving..."
+  captcha_file_name = "#{ENV['USERPROFILE']}\\citation\\magicyellow_captcha.png"
+  image_element.save captcha_file_name
+  s = CAPTCHA.solve captcha_file_name, :manual
+  puts "captcha solution #{s}" 
+  s
+end 
+
+def add_new_business(data)
+  @browser.text_field(:name => 'regNameRelation').set 'Owner'
   @browser.text_field(:name => 'OwnerNameFirst').set data[ 'first_name']
   @browser.text_field(:name => 'OwnerNameLast').set data[ 'last_name']
   @browser.text_field(:name => 'ContactPhone').set data[ 'phone']
   @browser.text_field(:name => 'ContactEmail').set data[ 'email']
-  @browser.text_field(:name => 'ContactEmail2').set data[ 'email']
   @browser.text_field(:name => 'lName').set data[ 'business']
   @browser.text_field(:name => 'lContact').value = data[ 'fullname' ]
   @browser.text_field(:name => 'lEmail').set data[ 'email']
@@ -16,49 +53,61 @@ def add_new_business(data)
   @browser.text_field(:name => 'lCity').set data[ 'city']
   @browser.select_list(:name => 'lState').select data[ 'state']
   @browser.text_field(:name => 'lZip').set data[ 'zip']
-  @browser.execute_script("window.open('add-your-business-cat.cfm')")
-  sleep(5)
-	
+  @browser.text_field(:name => "websiteURL").set data['url']
+  #@browser.execute_script("window.open('add-your-business-cat.cfm')")
+  #sleep(5)
+
   #attach new window
-  @browser.window(:url,/add-your-business-cat/i).use do
-    @browser.text_field(:id=> "search").when_present.set data[ 'business_category']
-    @browser.button(:value => 'Search').click
-    @browser.link(:text => "#{data[ 'business_category' ]}").click		
-  end
-  @browser.text_field(:name => 'describeBiz').set data[ 'business_description'] 
+  #@browser.window(:url,/add-your-business-cat/i).use do
+  #  @browser.text_field(:id=> "search").when_present.set data[ 'business_category']
+  #  @browser.button(:value => 'Search').click
+  #  @browser.link(:text => "#{data[ 'business_category' ]}").click	
+  #end
+  
+  @browser.text_field(:name => 'describebiz').set data[ 'business_description']
   @browser.checkbox(:name => 'optin').set
-  @browser.button(:value => 'Submit').click
-	
- #Check for error
-  if @browser.text.include?('Oops!...')
+
+  3.times do 
+    captcha = captcha_text(@browser.image(:src => /captcha.cfm/))
+    @browser.text_field(:name => 'captchaText').set captcha 
+    @browser.execute_script('document.getElementsByName("Submit")[0].click();')
+
+    puts "sleeping for 15"
+    sleep(15)
+    @browser.refresh unless @browser.td(:id => 'footer_logo').exist?
+
+    break unless @browser.text.include? "did not match"
+  end 
+
+  unless @browser.text.include? "Services, Products, Brands"
     throw "Somethign went wrong while filling basic info"
   end
-	
- # Second Step
-  @browser.text_field(:name => 'ServicesExtra').set data[ 'additional_services' ] 
-  @browser.text_field(:name => 'ProductsExtra').set data[ 'additional_products' ] 
-  @browser.text_field(:name => 'BrandsExtra').set data[ 'additional_brands' ] 
+
+  @browser.text_field(:name => 'ServicesExtra').set data[ 'additional_services' ]
+  @browser.text_field(:name => 'ProductsExtra').set data[ 'additional_products' ]
+  @browser.text_field(:name => 'BrandsExtra').set data[ 'additional_brands' ]
+
+  @browser.checkboxes(:name => 'biz_payment_option_type').each{|cb| cb.clear}
+  data['payment_option'].each do |p|
+    @browser.checkbox(:name => 'biz_payment_option_type', :value => p).set
+  end 
   
-  # Add payment_options
-  payments = data['payment_option']
-  payments.each do |payment|
-    next if card_type(payment).to_s == ""
-    @browser.checkbox(:value => card_type(payment).to_s).click
-  end
   @browser.button(:value => 'Submit').click
-  @success_msg = 'Congratulations, you have claimed your business and created a profile page.'
+  @success_msg = 'Thank you for claiming'
   if @browser.text.include?(@success_msg)
     puts "Initial registration is Successful"
 
-  if @chained
-    self.start("Magicyellow/Verify")
-  end
+    if @chained
+      self.start("Magicyellow/Verify")
+    end
+    self.success
 
-true
+    true
   else
     throw "Initial registration is not Successful"
   end
 end
+
 
 #Main Steps
 @browser.goto("http://www.magicyellow.com/add-your-business.cfm")

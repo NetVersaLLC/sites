f5d9366e2db54f7da29ad3bfc5e407e9bd12a027
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-12 15:15:08 -0500

    Update data_generator.rb

--- Yellowbot/CreateListing/data_generator.rb
+++ Yellowbot/CreateListing/data_generator.rb
@@ -4,7 +4,6 @@ data[ 'email' ] 		= business.yellow_bots.first.email
 data[ 'username' ] 		= data[ 'email' ]
 data[ 'password' ] 		= business.yellow_bots.first.password
 data[ 'phone' ] 		= business.local_phone
-# data[ 'link' ] 		= YellowBot.check_email(business)
 data[ 'business' ]		= business.business_name
 data[ 'phone' ]		= business.local_phone
 data[ 'address' ]		= business.address + ' ' + business.address2
@@ -14,5 +13,5 @@ data[ 'state' ]		= business.state_name
 data[ 'zip' ]			= business.zip
 data[ 'tollfree_number' ]	= business.toll_free_phone
 data[ 'website' ]		= business.company_website
-data[ 'hours_open' ]		= Getfav.consolidate_hours( business )
+data[ 'hours_open' ]		= YellowBot.consolidate_hours( business )
 data

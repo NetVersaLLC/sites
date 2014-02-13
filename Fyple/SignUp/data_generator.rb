5abc0405e63bcf6ca86dbf94a66a9f4f64234df6
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-13 15:06:53 -0500

    Update data_generator.rb

--- Fyple/SignUp/data_generator.rb
+++ Fyple/SignUp/data_generator.rb
@@ -1,7 +1,7 @@
 data = {}
-seed = rand(10000).to_s()
 data[ 'email' ]			   = business.bings.first.email
 data[ 'bing_password'] = business.bings.first.password
-data[ 'password' ]		 = Yahoo.make_password[0 .. 8]
+data[ 'password' ]		 = business.fyples.first.password || Yahoo.make_password[0 .. 8]
 data[ 'name'] = business.contact_first_name + ' ' + business.contact_last_name
 data
+

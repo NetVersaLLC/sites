30b568d9d1983d0300b3297f93e1b28448033165
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-14 15:43:38 -0500

    Update data_generator.rb

--- Yellowbot/Verify/data_generator.rb
+++ Yellowbot/Verify/data_generator.rb
@@ -2,5 +2,6 @@ data = {}
 data[ 'email' ] 		= business.yellow_bots.first.email
 data[ 'username' ] 		= data[ 'email' ]
 data[ 'password' ] 		= business.yellow_bots.first.password
-data['url']   = YellowBot.check_email(business)
+data[ 'bing_password']          = business.bings.first.password
+#data['url']   = YellowBot.check_email(business)
 data

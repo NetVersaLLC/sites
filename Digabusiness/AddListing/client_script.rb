f72036c30f2129dd04245f53697a000a45ce9db3
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-14 12:35:17 -0500

    better error checking.

--- Digabusiness/AddListing/client_script.rb
+++ Digabusiness/AddListing/client_script.rb
@@ -35,12 +35,21 @@ def add_listing( data )
     @browser.checkbox( :id => /#{pay}/i).click
   end 
 
-  enter_captcha( data )
-end 
+  3.times do 
+    @browser.text_field( :id => 'CAPTCHA').set solve_captcha
+    @browser.button( :name => 'submit').click
+
+    if @browser.span(:text => /URL could not be validated/).exist? || @browser.span(:text => /Invalid URL/).exist?
+      raise("Website provided is invalid.")
+    end
+    break if @browser.span(:text => /Domain already exists/) 
+    break unless @browser.span(:text => /Invalid code/).exist?
+  end
+
+end
 
 def solve_captcha	
   image = "#{ENV['USERPROFILE']}\\citation\\digabusiness_captcha.png"
-  puts("image saved?")
   obj = @browser.img( :title, 'Visual Confirmation Security Code' )
   puts("image saved.")
   puts "CAPTCHA source: #{obj.src}"
@@ -50,34 +59,9 @@ def solve_captcha
   CAPTCHA.solve image, :manual
 end
 
-def enter_captcha( data )
-	capSolved = false
-	count = 1
-	until capSolved or count > 5 do
-
-		captcha_code = solve_captcha
-
-		puts(captcha_code)
-		@browser.text_field( :id => 'CAPTCHA').set captcha_code
-		@browser.button( :name => 'submit').click
-		sleep(2)
-		if not @browser.text.include? "Invalid code."
-			capSolved = true
-		elsif @browser.text.include? "The URL could not be validated. Either the page does not exist or the server could not be contacted."
-			raise "Website provided is invalid."
-		end		  
-	count+=1
-	end
-	if capSolved == true
-		true
-	else
-		throw("Captcha was not solved")
-	end
-end
-
 sign_in(data)
 add_listing(data)
 
-Watir::Wait.until { @browser.text.include? "We got your submission!"}
 self.save_account("Digabusiness", {:status => "Listing created successfully!"})
 true
+

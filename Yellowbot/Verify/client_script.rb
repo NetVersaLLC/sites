5aa6d2fe8b89275fcc56bad3c3d3dc97dfe657af
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-14 15:43:07 -0500

    Update client_script.rb

--- Yellowbot/Verify/client_script.rb
+++ Yellowbot/Verify/client_script.rb
@@ -5,14 +5,44 @@ at_exit do
   end
 end
 
-@browser.goto(data['url'])
+def click_email_link(data)
+  @browser.goto("https://mail.live.com/") 
+  @browser.text_field(:name => "login").set data['email']
+  @browser.text_field(:name => 'passwd').set data['bing_password']
+  @browser.form(:name => "f1").button.click
+
+  puts 'polling afaor verification email'
+  quit_time = Time.now + 5 * 60;
+  until @browser.ul(:class => "InboxTableBody").link(:text => /\[YellowBot\] Confirm/).exists?
+    return false if Time.now > quit_time
+    folder = (folder == /Junk/ ? /Inbox/ : /Junk/)
+    sleep 5
+    puts 'switching folder and checking' 
+    @browser.ul(:id => "folderListControlUl").link(:title => folder).click 
+  end
+
+  puts 'email found'
+  @browser.ul(:class => "InboxTableBody").link(:text => /\[YellowBot\] Confirm/).click
+  @browser.div(:text => /confirm your email/).link(:text => /yellowbot/).when_present.click
+
+  puts 'clicked link.  waiting for 2nd window'
+  Watir::Wait.until do 
+    @browser.windows.size > 1 && @browser.windows[1].url.include?("yellowbot")
+  end 
+
+  @browser.window(:url => /yellowbot/).use
+end 
+
+click_email_link(data)
+
 @browser.text_field( :name => 'login').set data['email']
 @browser.text_field( :name => 'password').set data['password']
 @browser.button( :name => 'subbtn').click
-sleep(5)
+
+@browser.p(:text => /email address is now confirmed/).wait_until_present
 
 if @chained
-	  self.start("Yellowbot/CheckListing")
-	end
+  self.start("Yellowbot/CheckListing")
+end
 
-true
+self.success

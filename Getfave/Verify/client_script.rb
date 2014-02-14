ed3febc87aac78bdd86dbcbf53a1a23f00dfa505
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-14 13:37:21 -0500

    get mail directly.

--- Getfave/Verify/client_script.rb
+++ Getfave/Verify/client_script.rb
@@ -4,22 +4,36 @@ at_exit{
         @browser.close
     end
 }
+def check_email data
+  @browser.goto("https://mail.live.com/") 
+  @browser.text_field(:name => "login").set data['email']
+  @browser.text_field(:name => 'passwd').set data['bing_password']
+  @browser.form(:name => "f1").button.click
 
-link = data['link']
-if not link.nil?
-    @browser.goto(link)
-    if @browser.text.include? "Log Out"
-	   if @chained
-  		    self.start("Getfave/CreateListing")
-	   end
-	   self.save_account("Getfave", {:status => "Account verified successfully. Creating listing..."})
-  	   self.success
-    else
-	   raise "Error while email verification"
-    end
+  if @browser.ul(:class => "InboxTableBody").link(:text => /Activate Your Fave Account/).exist?
+    @browser.ul(:class => "InboxTableBody").link(:text => /Activate Your Fave Account/).click
+    @browser.link(:text => "go here").href
+  else 
+    nil
+  end
+end 
+
+
+link = check_email(data)
+if link.nil?
+  puts "Email not found, re-chaining..."
+  if @chained
+    self.start("Getfave/Verify", 1440)
+  end
 else
-    puts "Email not found, re-chaining..."
+  @browser.goto(link)
+  if @browser.link(:text => "Log Out").exist?
     if @chained
-        self.start("Getfave/Verify", 1440)
+      self.start("Getfave/CreateListing")
+    end
+    self.save_account("Getfave", {:status => "Account verified successfully. Creating listing..."})
+    self.success
+    else
+      raise "Error while email verification"
     end
 end

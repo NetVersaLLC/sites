4395c4757379412cd2b0b38ab4d93d41ce9b2b53
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-18 15:12:04 -0500

    clenaer captcha

--- Freebusinessdirectory/SignUp/client_script.rb
+++ Freebusinessdirectory/SignUp/client_script.rb
@@ -5,41 +5,14 @@ at_exit do
   end
 end
 
-def solve_captcha( obj )
-  image = ["#{ENV['USERPROFILE']}",'\citation\site_captcha.png'].join
-  puts "CAPTCHA source: #{obj.src}"
-  puts "CAPTCHA width: #{obj.width}"
-  obj.save image
-
+def solve_captcha
+  img = @browser.image(:id => 'validn_img')
+  image = ["#{ENV['USERPROFILE']}",'\citation\fbd_captcha.png'].join
+  puts "CAPTCHA source: #{img.src}"
+  puts "CAPTCHA width: #{img.width}"
+  img.save(image)
+  sleep 1
   CAPTCHA.solve image, :manual
-end
-
-@password = data['password']
-
-def enter_captcha( button, field, image, successTrigger, failureTrigger=nil )
-  capSolved = false
-  count = 1
-  until capSolved or count > 5 do
-    captcha_code = solve_captcha(image)
-    @browser.text_field(:id => 'pass2').set @password
-    @browser.text_field(:id => 'pass1').set @password
-    field.set captcha_code
-    button.click
-
-    30.times{ break if @browser.status == "Done"; sleep 1}
-    
-    unless failureTrigger.nil? or @browser.text.include?( failureTrigger)
-      capSolved = true
-    end
-    
-  count+=1  
-  end
-  if capSolved == true
-    Watir::Wait.until { @browser.text.include? successTrigger }
-    true
-  else
-    throw("Captcha was not solved")
-  end
 end
 
 @browser.goto( 'http://freebusinessdirectory.com/signup.php' )
@@ -49,19 +22,31 @@ end
 @browser.text_field(:id => 'lastname').set data['contact_last_name']
 @browser.text_field(:id => 'email').set data['email']
 @browser.text_field(:id => 'user_id').set data['userid']
-#@browser.text_field(:id => 'pass2').set data['password']
-#@browser.text_field(:id => 'pass1').set data['password']
-image = @browser.image(:id => 'validn_img')
-captcha = @browser.text_field(:id => 'validn_code')
-submit = @browser.button(:id => 'newcompany')
 
- #This one is weird; success returns a long SMTP log
-enter_captcha(submit,captcha,image,"SMTP")
+5.times do
+  @browser.text_field(:id => 'pass2').set data['password']
+  @browser.text_field(:id => 'pass1').set data['password']
+  @browser.text_field(:id => 'validn_code').set( solve_captcha )
 
-self.save_account("Freebusinessdirectory", { :userid => data['userid'], 
-  :email => data['email'], :password => data['password']})
-  
-if @chained
-  self.start("Freebusinessdirectory/Verify")
+  @browser.button(:id => 'newcompany').click
+
+  Watir::Wait.until do 
+    @browser.td(:text => /Incorrect validation code/).exist? || 
+      @browser.text.include?("SMTP")
+  end 
+  break if @browser.text.include?("SMTP")
 end
-true
+
+if @browser.text.include?("SMTP")
+  self.save_account("Freebusinessdirectory", { :username => data['userid'], 
+    :email => data['email'], :password => data['password']})
+    
+  if @chained
+    self.start("Freebusinessdirectory/Verify")
+  end
+  true
+else 
+  self.failure("captcha failed")
+end 
+
+

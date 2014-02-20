9bec84621ad59c2961dc357ee17cf6cf5d85d1ad
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-20 10:11:10 -0500

    email update

--- Mycitybusiness/Verify/client_script.rb
+++ Mycitybusiness/Verify/client_script.rb
@@ -1,18 +1,35 @@
 @browser = Watir::Browser.new :firefox
-at_exit {
-	unless @browser.nil?
-		@browser.close
-	end
+at_exit{
+    unless @browser.nil?
+        @browser.close
+    end
 }
 
-if data['url'].nil? then
-	if @chained
-		self.start("Mycitybusiness/Verify", 1440)
-		true
-	end
+def get_url_from_email data
+  @browser.goto("https://mail.live.com/") 
+  @browser.text_field(:name => "login").set data['email']
+  @browser.text_field(:name => 'passwd').set data['bing_password']
+  @browser.form(:name => "f1").button.click
+
+  if @browser.ul(:class => "InboxTableBody").link(:text => /myCity Business Activation/).exist?
+    @browser.ul(:class => "InboxTableBody").link(:text => /myCity Business Activation/).click
+    @browser.div(:text => /activate/).wait_until_present
+    @browser.link(:text => /activate/).href
+  else 
+    nil
+  end
+end 
+
+
+
+url = get_url_from_email(data)
+if url.nil?
+  if @chained
+    self.start("Mycitybusiness/Verify", 1440)
+  end
 else
-	@browser.goto(data['url'])
-	if @browser.text.include? "Thank you for adding your business to mycityBusiness.net."
-	  true
-	end
+  self.save_account("Mycitybusiness", {:listing_url => url} )
+  @browser.goto(url)
+  @browser.p(:text => /Thank you for adding/).wait_until_present
 end
+self.success

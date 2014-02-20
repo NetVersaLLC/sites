ab253f7ae7d1cbe1020660e007f78c3a9f3da886
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-20 17:55:20 -0500

    Update client_script.rb

--- Discoverourtown/AddListing/client_script.rb
+++ Discoverourtown/AddListing/client_script.rb
@@ -1,62 +1,61 @@
-# Developer's Notes
-# nil
-
-# Browser Code
 @browser = Watir::Browser.new :firefox
-at_exit{
-  unless @browser.nil?
-    @browser.close
-  end
-}
+at_exit do
+	unless @browser.nil?
+		@browser.close
+	end
+end
 
-# Methods
-def solve_captcha
-  image = "#{ENV['USERPROFILE']}\\citation\\discoverourtown_captcha.png"
-  obj = @browser.image( :src, /recaptcha/ )
-  puts "CAPTCHA source: #{obj.src}"
+def solve_captcha2
+  image = ["#{ENV['USERPROFILE']}",'\citation\disc_captcha.png'].join
+  obj = @browser.img( :id => "recaptcha_challenge_image")
   puts "CAPTCHA width: #{obj.width}"
   obj.save image
-  CAPTCHA.solve image, :manual
+  sleep(3)
+  return CAPTCHA.solve image, :manual
 end
 
 def enter_captcha
-  capRetries ||= 3
-  @browser.text_field(:name, 'recaptcha_response_field').set solve_captcha
-  @browser.button(:value, 'Submit').click
-  if @browser.url.include? 'failed=true'
-    raise "Captcha failed"
-  end
-rescue => e
-    retry unless (capRetries -= 1).zero?
-    self.failure(e)
+  3.times do
+    captcha_code = solve_captcha2	
+    @browser.text_field(:id => "recaptcha_response_field").set captcha_code
+    @browser.input(:value => "Submit").click
+
+    Watir::Wait.until do 
+      @browser.form(:text => /response was incorrect/).exist? || 
+      @browser.h1(:text => "Select Your Listing").exist?
+    end 
+    return true if @browser.h1(:text => "Select Your Listing").exist?
+  end 
+  return false
 end
 
-def post_listing(data)
-  retries ||= 3
-  @browser.goto('http://www.discoverourtown.com/add/')
-  # Fillout form
-  @browser.text_field(:name, 'ListContact' ).set     data['name']
-  @browser.text_field(:name, 'ReqEmail' ).set        data['email']
-  @browser.text_field(:name, 'ListOrgName' ).set     data['business']
-  @browser.text_field(:name, 'ListAddr1' ).set       data['address']
-  @browser.text_field(:name, 'ListCity' ).set        data['city']
-  @browser.text_field(:name, 'ListState' ).set       data['state']
-  @browser.text_field(:name, 'ListZip' ).set         data['zip']
-  @browser.text_field(:name, 'ListPhone' ).set       data['phone']
-  @browser.text_field(:name, 'ListTollFree' ).set    data['tollfree']
-  @browser.text_field(:name, 'ListWebAddress' ).set  data['website']
-  @browser.textarea(:name, 'ListStatement' ).set     data['description']
-  # Captcha
-  enter_captcha()
-  @browser.link(:href => 'thankyou.php').when_present.click
-  Watir::Wait.until { @browser.text.include? 'Thank you for Your Submission' }
-rescue => e
-  puts e
-  retry unless (retries -= 1).zero?
-  self.failure(e)
-else
-  self.save_account("Discoverourtown", {:status => "Listing posted!" })
-  self.success
+# End Temporary Methods from Shared.rb
+def add_listing(data)
+	@browser.text_field(:name => 'ListContact').when_present.set data[ 'full_name' ]
+	@browser.text_field(:name => 'ReqEmail').when_present.set data[ 'email' ]
+	@browser.text_field(:name => 'ListOrgName').set data[ 'business' ]
+	@browser.text_field(:name => 'ListAddr1').set data[ 'address' ]
+	@browser.text_field(:name => 'ListCity').set data[ 'city' ]
+	@browser.text_field(:name => 'ListState').set data[ 'state' ]
+	@browser.text_field(:name => 'ListZip').set data[ 'zip' ]
+	@browser.text_field(:name => 'ListPhone').set data[ 'phone' ]
+	@browser.text_field(:name => 'ListWebAddress').set data[ 'website' ]
+	@browser.text_field(:name => 'ListStatement').set data[ 'business_description' ]
+
+	#Enter Decrypted captcha string here
+	return false unless enter_captcha
+
+	@browser.link(:href => 'thankyou.php').click
+        @browser.p(:text => /submission was successful/).wait_until_present
+	true
 end
-# Controller
-post_listing(data)
+
+url = 'http://www.discoverourtown.com/add/'
+@browser.goto(url)
+
+if add_listing(data)
+  self.save_account("Discoverourtown", {:email => data[ 'email' ], :status => "Listing created. Waiting for approval"})
+  self.success
+else 
+  self.failure("Captcha failed")
+end 

de0fa3ec87dbd8ba6c7947d4ba483b803afaf2d0
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-24 11:29:17 -0500

    Update client_script.rb

--- Bing/SignUp/client_script.rb
+++ Bing/SignUp/client_script.rb
@@ -20,8 +20,8 @@ end
 def solve_captcha2
   begin
   image = "#{ENV['USERPROFILE']}\\citation\\bing1_captcha.png"
-  obj = @browser.img( :xpath, '//div/table/tbody/tr/td/img[1]' )
-  puts "CAPTCHA source: #{obj.src}"
+  obj = @browser.div(:id => 'WLX_HIP_CTL_AMFE').img()
+  #obj = @browser.img( :xpath, '//div/table/tbody/tr/td/img[1]' )
   puts "CAPTCHA width: #{obj.width}"
   obj.save image
   sleep(3)
@@ -33,59 +33,21 @@ def solve_captcha2
 end
 
 def enter_captcha
-  captcharetries = 5
-  capSolved = false
- until capSolved == true || captcharetries == 0
+  5.times do
     captcha_code = solve_captcha2 
-    @browser.execute_script("
-      function getRealId(partialid){
-        var re= new RegExp(partialid,'g')
-        var el = document.getElementsByTagName('*');
-        for(var i=0;i<el.length;i++){
-          if(el[i].id.match(re)){
-            return el[i].id;
-            break;
-          }
-        }
-      }
-      
-      _d.getElementById(getRealId('wlspispSolutionElement')).value = '#{captcha_code}';
+    obj = @browser.div(:id => 'WLX_HIP_CTL_AMFE').text_field().set captcha_code
+    @browser.button(:value => /Create account/).click
 
-      ")
-      sleep(5)
+    sleep(20)
 
-      @browser.execute_script('
-        jQuery("#SignUpForm").submit()
-      ')
-
-      sleep 15
-
-    if @browser.url =~ /https:\/\/account.live.com\/summarypage.aspx/i
-      capSolved = true
-    elsif @browser.text.include? "The email address or single-use code is incorrect. Please try again."
-      capSolved = true
-    else
-      captcharetries -= 1
-    end
-    if capSolved == true
-      break
-    end
-
+    break if @browser.url =~ /https:\/\/account.live.com\/summarypage.aspx/i
   end
 
-  if capSolved == true
-    return true
-  else
-    throw "Captcha could not be solved"
-  end
-   
-
+  raise("Captcha could not be solved") unless @browser.url =~ /https:\/\/account.live.com\/summarypage.aspx/i
 end
 
-# End Temporary Methods from Shared.rb
 
 if data['gender'] == "Unknown" then data['gender'] = "Male" end
-puts data
 
 @browser.goto('https://signup.live.com/signup.aspx?')
 
@@ -93,55 +55,13 @@ puts data
 retries = 3
 type = ""
 begin
-
-  case type
-
-    #Use raw javascript to enter the fields
-  when "javascript"
-    @browser.execute_script("
-      _d.getElementById('iFirstName').value = '#{data[ "first_name" ]}';
-      _d.getElementById('iLastName').value = '#{data[ "last_name" ]}';
-      _d.getElementById('iBirthMonth').value = '#{data[ "birth_month" ]}';
-      _d.getElementById('iBirthDay').value = '#{data[ "birth_day" ]}';
-      _d.getElementById('iBirthYear').value = '#{data[ "birth_year" ]}';
-      _d.getElementById('iGender').value = 'u';
-    ")
-
-    @browser.execute_script("
-      jQuery('#iliveswitch').trigger('click')
-    ")
-    sleep(4)
-
-    @browser.execute_script("
-      _d.getElementById('iAltEmail').value = '#{data[ "alt_email" ]}';
-      _d.getElementById('iPhone').value = '#{data[ "mobile_phone" ]}';
-      _d.getElementById('iCountry').value = '#{data[ "country" ]}';
-      _d.getElementById('iZipCode').value = '#{data["zip"]}';
-      _d.getElementById('iOptinEmail').checked = false;
-
-      _d.getElementById('iPwd').value = '#{data[ "password" ]}';
-      _d.getElementById('iRetypePwd').value = '#{data[ "password" ]}';
-    ")
-  
-    email_name = data[ 'name_longer' ].downcase.delete( ' ' ).strip + (rand( 10000 )+200).to_s #get_email_name( data )
-
-    @browser.execute_script("
-      _d.getElementById('imembernamelive').value = '#{email_name}';
-    ")
-
-    data[ 'hotmail' ] = email_name + '@outlook.com'
-    puts(data['hotmail'])
-  
-    
-
-  else
-    @browser.text_field(  :xpath, '//*[@id="iFirstName"]' ).set data[ "first_name" ]
-    @browser.text_field(  :id, /iLastName/ ).set data[ "last_name" ]
+    @browser.text_field(  :id, 'iFirstName' ).set data[ "first_name" ]
+    @browser.text_field(  :id, 'iLastName' ).set data[ "last_name" ]
     @browser.select_list( :id, /iBirthMonth/ ).when_present.select_value data[ 'birth_month' ]
     @browser.select_list( :id, /iBirthDay/ ).select   data[ 'birth_day' ]
     @browser.select_list( :id, /iBirthYear/ ).select  data[ 'birth_year' ]
     @browser.select_list( :id, /iGender/ ).select     data[ 'gender' ]
-    # click <get a new email address> to open alt email field
+
     @browser.link( :id, /iliveswitch/ ).click
     sleep 4 # or wait until id => imembernamelive exists
     @browser.text_field( :id, /iAltEmail/ ).set    data[ 'alt_email' ]
@@ -207,44 +127,7 @@ begin
       end
     end
     data[ 'hotmail' ] = email_name + '@outlook.com'
-  end
 
-  
-
-rescue Selenium::WebDriver::Error::ElementNotVisibleError
-   if retries > 0
-    puts("Watir cannot find the element, trying again in 3 seconds. #{retries} remaining.")
-    sleep 3
-    retries -= 1
-    retry
-  else
-    puts("Watir cannot find the element, switching to Javascript.")
-    type = "javascript"
-    retries = 3
-    retry
-  end
-
-rescue Selenium::WebDriver::Error::JavascriptError
-  if retries > 0
-    puts("The javascript failed, trying again in 3 seconds. #{retries} remaining.")
-    sleep 3
-    retries -= 1
-    retry
-  else
-    puts("The javascript failed and we are out of options.")
-  end
-
-rescue Exception => e
-    puts "Caught a #{e.class}"
-  if retries > 0
-    puts("Unhandled error: #{e.class}")
-    puts("Trying again in 3 seconds.")
-    sleep 3
-    retries -= 1
-    retry
-  elsif
-    throw("Job has failed with an unhandled error: #{e.class}")
-  end
 
 end
 
@@ -252,16 +135,13 @@ end
 sleep(3)
 
 data['email'] = data['hotmail']
-puts data['hotmail']
-puts data['password']
 
 enter_captcha
 
-    #Watir::Wait.until { @browser.text.include? "Account summary" }
-    self.save_account('Bing',  {:email => data['email'],:password => data['password'], :status => "Account created, creating listing..."})
+self.save_account('Bing',  {:email => data['email'],:password => data['password'], :status => "Account created, creating listing..."})
 
 if @chained
   self.start("Bing/CreateRule")
 end
 
-true
+self.success

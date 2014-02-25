d9ab39e66eb29b8e4bec0c687265f9ed61c6e7a4
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-25 14:15:39 -0500

    Update client_script.rb

--- Magicyellow/Verify/client_script.rb
+++ Magicyellow/Verify/client_script.rb
@@ -11,15 +11,11 @@ def get_password_from_email(data)
   @browser.text_field(:name => 'passwd').set data['bing_password']
   @browser.form(:name => "f1").button.click
 
-  puts 'polling afaor verification email'
-  quit_time = Time.now + 5 * 60;
-  until @browser.ul(:class => "InboxTableBody").link(:text => /MagicYellow.com Business Center Login/).exist?
-    return false if Time.now > quit_time
-    folder = (folder == /Junk/ ? /Inbox/ : /Junk/)
-    sleep 5
-    puts 'switching folder and checking'
-    @browser.ul(:id => "folderListControlUl").link(:title => folder).click
-  end
+  30.times do
+    sleep(2)
+    break if @browser.ul(:class => "InboxTableBody").link(:text => /MagicYellow.com Business Center Login/).exist?
+  end 
+  return nil unless @browser.ul(:class => "InboxTableBody").link(:text => /MagicYellow.com Business Center Login/).exist?
 
   puts 'email found'
   @browser.ul(:class => "InboxTableBody").link(:text => /MagicYellow.com Business Center Login/).click
@@ -29,9 +25,16 @@ end
 
 password = get_password_from_email(data)
 
-self.save_account("Magicyellow", {:email => data['email'], :password => password })
-if @chained
-  self.start("Magicyellow/UpdateListing")
+if password 
+	self.save_account("Magicyellow", {:email => data['email'], :password => password })
+	if @chained
+	  self.start("Magicyellow/UpdateListing")
+	end
+else 
+	if @chained
+	  self.start("Magicyellow/Verify", 60)
+	end
 end
+
 self.success
 

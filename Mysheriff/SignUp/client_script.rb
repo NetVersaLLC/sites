b6fa93ddd1721b3501732502f579b0fbf39457b8
Ted Palmisciano <tpalmisciano@gmail.com>
2014-02-26 16:45:05 -0500

    bug fixes and convert to watir.

--- Mysheriff/SignUp/client_script.rb
+++ Mysheriff/SignUp/client_script.rb
@@ -1,72 +1,64 @@
-# Developer's Notes
-# Good idea for listing site: mydeputy.net
+@browser = Watir::Browser.new :firefox
 
-# Agent Code
-agent = Mechanize.new
-agent.user_agent_alias = 'Windows Mozilla'
-
-# Classes
-class IncorrectCaptcha < StandardError
-end
-
-# Methods
-def solve_captcha(page)
-  captcha = page.image_with(:src => /verificationimage/) # Finds the captcha image
-  captcha.fetch.save! "#{ENV['USERPROFILE']}\\citation\\mysheriff_captcha.png" # Saves it to the user's computer
-  image = "#{ENV['USERPROFILE']}\\citation\\mysheriff_captcha.png" # Do not combine with above line, sets the image variable to the local image file
-  captcha_text = CAPTCHA.solve image, :manual # Solves the captcha
-  return captcha_text # Returns the solve to enter_captcha method
-end
-
-def enter_captcha(page, form, data)
-  capRetries ||= 5 # Sets total retries once
-  form.verif_box = solve_captcha(page) # Enters solved captcha data into the captcha field
-  page = form.click_button(form.button_with(:type => 'submit')) # Submits the form
-  if page.body.include?("Please Enter the text in the image Correctly.") # Checks to see if we failed the captcha
-    raise IncorrectCaptcha
-  elsif page.body.include? 'Nice to see you today!' # Otherwise, checks to see if we're on the right page afterwards
-    true
-  else # Fail if we get an unhandled page or error
-    raise "Unexpected result after captcha."
+at_exit {
+  unless @browser.nil?
+    @browser.close
   end
-rescue IncorrectCaptcha
-  retry unless (capRetries -= 1).zero?
+}
+
+def signup(data) 
+  @browser.goto "http://www.mysheriff.net/users/"
+
+  @browser.radio(:value => data['gender']).set
+
+  add_user_form = @browser.form(:id => 'AddUser') 
+
+  @browser.text_field(:name => 'firstname').set     data['first_name']
+  @browser.text_field(:name => 'lastname').set      data['last_name']
+  @browser.text_field(:name => 'email_address').set data['email']
+
+  @browser.select_list(:name=>'cmbmonth').select    data['birthday_month']
+  @browser.select_list(:name=>'cmbday').select      data['birthday_day']
+  @browser.select_list(:name=>'cmbyear').select     data['birthday_year']
+
+  # city displays a drop down that you need to select from
+  @browser.text_field(:name => 'City').set          data['city']
+  @browser.div(:class => 'ac_results', :text => /#{data['city']}/).wait_until_present
+  @browser.div(:class => 'ac_results').li(:text => /#{data['state']}/).click
+
+  3.times do 
+    add_user_form.text_field(:id   => 'password').set      data['password']
+    @browser.text_field(:name => 'verif_box').set     solve_catcha( @browser.form(:id => 'AddUser').image )
+    @browser.button(:value => 'Sign Up').click
+
+    break if @browser.h1(:text => /Nice to see you/).exist?
+    break if @browser.li(:text => /email address has already been used/).exist?
+  end 
+  @browser.h1(:text => /Nice to see you/).exist? || @browser.li(:text => /email address has already been used/).exist?
+end 
+
+def solve_captcha(image_element)
+  image_file = "#{ENV['USERPROFILE']}\\citation\\mysheriff_captcha.png"
+  puts "CAPTCHA width: #{image_element.width}"
+  image_element.save image_file
+  sleep(3)
+
+  return CAPTCHA.solve image_file, :manual
+end
+
+if signup(data) 
   if @chained
-    self.start("Mysheriff/SignUp")
+    self.start("Mysheriff/CreateListing")
+  end 
+  self.success
+else 
+  reg_error = @browser.div('reg_error')
+  if reg_error.exist? && reg_error.lis.count == 1 && reg_error.text.include?('image Correctly') 
+    if @chained
+      self.start("Mysheriff/SignUp", 1440) 
+    end 
+    self.failure "captcha failed. trying again later"
+  else
+    raise "Error filling out form" 
   end
-  self.failure("Captcha failed, re-chaining...")
-
-rescue => e
-  retry unless (capRetries -= 1).zero? # Retry unless we've tried 5 times already
-  self.failure(e)
-else
-  if @chained
-    self.start("Mysheriff/AddListing")
-  end
-  self.save_account('Mysheriff', {:email => data['email'], :password => data['password'], :status => "Account created, posting listing..."})
-  self.success # Prevents self.success from being called in the event of a self.failure
 end
-
-def signup(data, agent)
-  # Goto webpage, grab form
-  page = agent.get('http://www.mysheriff.net/users/signup/')
-  form = page.form_with('Form1')
-  # Set gender radio button
-  form.radiobutton_with(:value => data['gender']).check
-  # Set basic information fields
-  form.firstname = data['first_name']
-  form.lastname = data['last_name']
-  form.email_address = data['email']
-  form.password = data['password']
-  form.City = data['city']
-  # Set birthday via select lists
-  form.field_with('cmbmonth').value = data['birthday_month']
-  form.field_with('cmbday').value = data['birthday_day']
-  form.field_with('cmbyear').value = data['birthday_year']
-  # Solve captcha
-  enter_captcha(page, form, data)
-end
-
-# Controller
-self.save_account('Mysheriff', {:status => "Account creation pending."})
-signup(data, agent)

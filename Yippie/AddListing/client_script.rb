13b1db71407c1b5992a3e5d934173fc25d7a01b0
Kazyyk <Asphexis@gmail.com>
2014-04-14 10:37:29 -0400

    Rewrite to Mechanize
    Rewrote the payload to utilize Mechanize for faster and more solid execution

--- Yippie/AddListing/client_script.rb
+++ Yippie/AddListing/client_script.rb
@@ -1,56 +1,36 @@
-@browser = Watir::Browser.new :firefox
-at_exit {
-	unless @browser.nil?
-		@browser.close
+# Agent
+agent = Mechanize.new
+agent.user_agent_alias = 'Windows Mozilla'
+
+#Methods
+
+def submit_listing(agent, data)
+	# Go to page
+	page = agent.get( 'http://submit.yippie.biz/' )
+	# Identify form
+	form = page.form_with('frmAddBusinessListing')
+	# Fill form
+	form.businessname = data['business']
+	form.address = data['addressComb']
+	form.city = data['city']
+	form.state = data['state']
+	form.zipcode = data['zip']
+	form.telephone1 = data['areacode']
+	form.telephone2 = data['exchange']
+	form.telephone3 = data['last4']
+	form.website = data['website']
+	form.email = data['email']
+	form.field_with('categories').value = data['category1']
+	# Submit form
+	page = form.click_button()
+	if page.body.include? "Thank you for submitting your FREE business listing to YiPpIe!"
+		self.success
+	else
+		self.failure
 	end
-}
-
-
-@browser.goto( 'http://submit.yippie.biz/' )
-
-@browser.text_field( :name => 'businessname').set data['business']
-@browser.text_field( :name => 'address').set data['addressComb']
-@browser.text_field( :name => 'city').set data['city']
-@browser.text_field( :name => 'state').set data['state']
-@browser.text_field( :name => 'zipcode').set data['zip']
-@browser.text_field( :name => 'telephone1').set data['areacode']
-@browser.text_field( :name => 'telephone2').set data['exchange']
-@browser.text_field( :name => 'telephone3').set data['last4']
-@browser.text_field( :name => 'website').clear
-@browser.text_field( :name => 'website').set data['website']
-@browser.text_field( :name => 'email').set data['email']
-@browser.select_list( :name => 'categories').click
-
-
-begin
-	@browser.select_list( :name => 'categories').option( :text => data['category1']).click
-	puts('category clicked')
-
-
-	rescue
-	puts("Selection failed. Trying alternative method in 3 seconds.")
-	sleep(3)	
-
-	@browser.select_list(:name => 'categories').option do |check|
-		if check =~ /:/ then
-				aftercolon = data['category1'].split(':').last
-				if aftercolon =~ check.text.split(':').last then
-				check.value.click
-				else
-					puts("Category Selection failed")
-		end			
-		else
-			puts('No colon here, moving on...')
-			end
-		end
-	end
-
-@browser.button( :value => 'Submit').click
-sleep 2
-
-if @browser.text.include? "Thank you for submitting your FREE business listing to YiPpIe!"
-	true
+rescue => e
+	puts e
 end
-#else
-	#throw("Payload Failure")
-#end
+
+# Main Controller
+submit_listing(agent, data)

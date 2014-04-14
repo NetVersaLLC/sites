f67a5cc6149f968e2bb137ab54e38f898211cda4
Kazyyk <Asphexis@gmail.com>
2014-04-14 11:04:52 -0400

    Added handling

--- Localpages/Verify/client_script.rb
+++ Localpages/Verify/client_script.rb
@@ -1,7 +1,11 @@
-self.save_account('Localpages', { :password => data['password'] })
+unless data['password'].nil?
+	self.save_account('Localpages', { :password => data['password'] })
 
-	if @chained
-		self.start("Localpages/AddListing")
-	end
+		if @chained
+			self.start("Localpages/AddListing")
+		end
 
-self.success
+	self.success
+else
+	self.start("Localpages/Verify", 1440)
+end

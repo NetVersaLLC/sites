e1f9042e424c567e1662999e788a78e743756288
vivekshine22 <vivekshine22@gmail.com>
2014-05-02 21:14:44 +0530

    Update data_generator.rb

--- Citygrid/AddListing/data_generator.rb
+++ Citygrid/AddListing/data_generator.rb
@@ -1,3 +1,13 @@
 data = {}
-
+data[ 'first_name' ]	= business.contact_first_name
+data[ 'last_name' ] 	= business.contact_last_name
+data['mobile-phone']  = business.mobile_phone
+data[ 'phone' ] 	= business.local_phone
+data[ 'email' ]		= business.bings.first.email
+data[ 'business_name' ] = business.business_name
+data[ 'zip' ] 		= business.zip
+data['address']  = business.address
+data['city'] = business.city
+data['website'] = business.company_website
+data['category'] = business.category3
 data

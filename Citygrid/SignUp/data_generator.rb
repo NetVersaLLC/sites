ce30ab3342ee45d633139158493dad084d29d71c
vivekshine22 <vivekshine22@gmail.com>
2014-05-02 21:08:06 +0530

    Update data_generator.rb

--- Citygrid/SignUp/data_generator.rb
+++ Citygrid/SignUp/data_generator.rb
@@ -1,7 +1,9 @@
 data = {}
 data[ 'first_name' ]	= business.contact_first_name
 data[ 'last_name' ] 	= business.contact_last_name
-data[ 'phone' ] 	= business.local_phone
+data[ 'area-code' ] 	= business.local_phone[0..2]
+data[ 'prefix' ] 	= business.local_phone[3..5]
+data[ 'number' ] 	= business.local_phone[6..9]
 data[ 'email' ]		= business.bings.first.email
 data[ 'password' ]	= Yahoo.make_password
 data[ 'business_name' ] = business.business_name

6f3820ce04dda3f347db8faa881e1d7e46c9368a
vivekshine22 <vivekshine22@gmail.com>
2014-05-05 17:24:26 +0530

    Update client_script.rb

--- Patch/SignUp/client_script.rb
+++ Patch/SignUp/client_script.rb
@@ -10,21 +10,26 @@ at_exit {
 @browser.text_field(:id => 'zip').set '90210' #data[ 'zip' ]
 @browser.button(:class => 'submit').click
 
-Watir::Wait.until { @browser.text.include? ("We’re not in your town quite yet, but we will be." or "We've got you covered! Here's your Patch:") }
+# Watir::Wait.until { @browser.text.include? ("We’re not in your town quite yet, but we will be." or "We've got you covered! Here's your Patch:") }
 
 if @browser.text.include? "We’re not in your town quite yet, but we will be."
 	puts "Patch not avilable for this business!"
 else
-	@browser.div(:class => "response").div.link.click
+	# @browser.div(:class => "response").div.link.click
+	@browser.link(:xpath,'/html/body/div/div/div/div[2]/div/div[2]/a').click
 	@browser.link(:class => 'js-trackable js-needs_current_user data-tag').click
-	@browser.text_field(:name => 'name').set 'John Smith'
-	@browser.text_field(:name => 'email').set 'wnvta123@live.com'
-	@browser.text_field(:name => 'password').set 'test123'
-	@browser.text_field(:name => 'confirm_password').set 'test123'
-	@browser.label(:class => 'checkbox').click
+	@browser.text_field(:name => 'name').set data['name']#'John Smith'
+        @browser.text_field(:xpath ,'/html/body/div[3]/div[2]/div[2]/form/div[2]/div/input').set data['email']#'wnvta123@live.com'
+
+	@browser.text_field(:xpath => '/html/body/div[3]/div[2]/div[2]/form/div[3]/div/input').set data['password'] #'test123'
+
+	@browser.text_field(:xpath => '/html/body/div[3]/div[2]/div[2]/form/div[4]/div/input').set data['password']#'test123'
+
+	# @browser.label(:class => 'checkbox').click
 	@browser.checkbox(:id => 'subscriptions_Daily').clear
 	@browser.button(:id => 'signup').click
-	@browser.button(:class => 'close close_modal').click
+	@browser.button(:xpath => '/html/body/div[3]/div/div[2]/button').click
+
 end
 
 true

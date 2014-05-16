7d2e2ca794794c973145ccdfc5679f3990a78e13
vivekshine22 <vivekshine22@gmail.com>
2014-05-16 21:13:00 +0530

    Update data_generator.rb

--- Citisquare/UpdateListing/data_generator.rb
+++ Citisquare/UpdateListing/data_generator.rb
@@ -2,9 +2,10 @@ data = {}
 catty = Citisquare.where(:business_id => business.id).first
 data['email'] = business.bings.first.email
 data['password'] = business.citisquares.first.password
-data['phone_area'] = business.local_phone.split("-")[0]
-data['phone_prefix'] = business.local_phone.split("-")[1]
-data['phone_suffix'] = business.local_phone.split("-")[2]
+# data['phone_area'] = business.local_phone.split("-")[0]
+# data['phone_prefix'] = business.local_phone.split("-")[1]
+# data['phone_suffix'] = business.local_phone.split("-")[2]
+data['phone'] = business.local_phone
 data['business'] = business.business_name
 data['state'] = business.state_name
 data['zip'] = business.zip
@@ -27,7 +28,7 @@ data['apt_number'] = business.address2[/\d+/]
 data['box_number'] = ''
 
 data['city'] = business.city
-data['category'] = catty.citisquare_category.parent.name.gsub("\n", "")
-data['sub_category'] = catty.citisquare_category.name.gsub("\n", "")
+# data['category'] = catty.citisquare_category.parent.name.gsub("\n", "")
+# data['sub_category'] = catty.citisquare_category.name.gsub("\n", "")
 data['specials'] = business.category1 + ' ' + business.category2 + ' ' + business.category3 + ' ' + business.category4 + ' ' + business.category5
 data

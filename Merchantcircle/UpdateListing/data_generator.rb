092d8c9ed97c31b60092fc10080458b4e880ba48
vivekshine22 <vivekshine22@gmail.com>
2014-05-23 21:08:41 +0530

    Update data_generator.rb

--- Merchantcircle/UpdateListing/data_generator.rb
+++ Merchantcircle/UpdateListing/data_generator.rb
@@ -1,10 +1,11 @@
 data = {}
 data[ 'email' ]			= business.merchantcircles.first.email
-data[ 'password' ]			= business.merchantcircles.first.password
-data[ 'phone_area' ] 		= business.local_phone.split("-")[0]
-data[ 'phone_prefix' ] 		= business.local_phone.split("-")[1]
-data[ 'phone_suffix' ] 		= business.local_phone.split("-")[2]
-data[ 'phone' ] 			= data[ 'phone_area'] + data[ 'phone_prefix'] + data[ 'phone_suffix']
+data[ 'password' ]		= business.merchantcircles.first.password
+# data[ 'phone_area' ] 		= business.local_phone.split("-")[0]
+# data[ 'phone_prefix' ] 		= business.local_phone.split("-")[1]
+# data[ 'phone_suffix' ] 		= business.local_phone.split("-")[2]
+# data[ 'phone' ] 			= data[ 'phone_area'] + data[ 'phone_prefix'] + data[ 'phone_suffix']
+data['phone']                           = business.local_phone
 data[ 'name' ]				= business.business_name
 data[ 'address' ] 			= business.address + ' ' + business.address2
 data[ 'city' ] 				= business.city
@@ -30,22 +31,27 @@ data[ 'satOpen' ]			= business.saturday_open
 data[ 'satClose' ]			= business.saturday_close
 data[ 'sunOpen' ]			= business.sunday_open
 data[ 'sunClose' ]			= business.sunday_close
-data[ 'keywords' ]			= business.category1 + ", " + business.category2 + ", " + business.category3 + ", " + business.category4 + ", " + business.category5 
+data[ 'keywords' ]			= business.category1 + ", " + business.category2 + ", " + business.category3 #+ ", " + business.category4 + ", " + business.category5 
 catty                   = Merchantcircle.where(:business_id => business.id).first
 
-if catty.merchantcircle_category.parent.parent.name
-	thecats = {
-		'levelone' => catty.merchantcircle_category.parent.parent.name,
-		'leveltwo' => catty.merchantcircle_category.parent.name,
-		'levelthree' => catty.merchantcircle_category.name 
-	};
-else
-	thecats = { 
-		'levelone' => catty.merchantcircle_category.parent.name,
-		'leveltwo' => catty.merchantcircle_category.name 
-		};
-end
+mer_cat                 = MerchantcircleCategory.find(catty.category_id)
 
+# if catty.merchantcircle_category.parent.parent.name
+# 	thecats = {
+# 		'levelone' => catty.merchantcircle_category.parent.parent.name,
+# 		'leveltwo' => catty.merchantcircle_category.parent.name,
+# 		'levelthree' => catty.merchantcircle_category.name 
+# 	};
+# else
+# 	thecats = { 
+# 		'levelone' => catty.merchantcircle_category.parent.name,
+# 		'leveltwo' => catty.merchantcircle_category.name 
+# 		};
+# end
+thecats = { 
+		'levelone' => mer_cat.parent.name,
+		'leveltwo' => mer_cat.name 
+		};
 data['thecategories'] = thecats
 
 data

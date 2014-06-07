fc1d108cf63e22e488b8ec6e95ae60fd8ab70daf
vivekshine22 <vivekshine22@gmail.com>
2014-06-07 20:34:49 +0530

    updated city location

--- Citisquare/ClaimListing/data_generator.rb
+++ Citisquare/ClaimListing/data_generator.rb
@@ -2,5 +2,10 @@ data = {}
 data['email'] = business.bings.first.email
 data['password'] = business.citisquares.first.password
 data['business'] = business.business_name
+data['city'] = 'Murray'
+data['state'] = business.state_name
 data['zip'] = business.zip
+data['phone'] = business.local_phone
+data['address'] = business.address
+data['location'] = data['city']+", "+data['state']+", "+data['zip']
 data

415da35ac0659fa71fcd8a6352dad2f069bf7fa7
vivekshine22 <vivekshine22@gmail.com>
2014-06-14 22:00:37 +0530

    updated data entries

--- Freebusinessdirectory/CreateListing/data_generator.rb
+++ Freebusinessdirectory/CreateListing/data_generator.rb
@@ -1,6 +1,6 @@
 data = {}
 data[ 'username' ]		= business.freebusinessdirectories.first.username
-data[ 'password' ]		= business.bings.first.password
+data[ 'password' ]		= business.freebusinessdirectories.first.password
 data[ 'description' ]		= business.business_description
 data[ 'website' ]		= business.company_website
 data[ 'salutation' ]		= business.contact_prefix

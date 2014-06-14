f8d23dd6841160c985f7ff9d77eeff78781159bb
vivekshine22 <vivekshine22@gmail.com>
2014-06-14 21:46:33 +0530

    updated user data

--- Freebusinessdirectory/Verify/data_generator.rb
+++ Freebusinessdirectory/Verify/data_generator.rb
@@ -1,5 +1,6 @@
 data = {}
 data[ 'email' ]	= business.freebusinessdirectories.first.email
 data[ 'password' ]	= business.freebusinessdirectories.first.password
+data[ 'userid' ] = business.bings.first.email[0 .. 15].gsub("@","")
 data[ 'bing_password']  = business.bings.first.password
 data
